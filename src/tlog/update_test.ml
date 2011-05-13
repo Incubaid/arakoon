171bc8fad3c6e79c6dc872aeef5adb97a5104434
Romain Slootmaekers <rslootma@b-virtual.org>
2011-05-13 10:32:54 +0200

    introducing node ranges

--- src/tlog/update_test.ml
+++ src/tlog/update_test.ml
@@ -22,7 +22,15 @@ If not, see <http://www.gnu.org/licenses/>.
 
 open OUnit
 open Update
+open Range
+let _b2b u = 
+  let b = Buffer.create 1024 in
+  let () = Update.to_buffer b u in
+  let flat = Buffer.contents b in
+  let u',_ = Update.from_buffer flat 0 in
+  u'
 
+let _cmp = OUnit.assert_equal ~printer:Update.string_of 
 let test_sequence () =
   let s = Update.Sequence [
     Update.make_master_set "Zen" None;
@@ -30,13 +38,20 @@ let test_sequence () =
     Update.Delete "key";
     Update.TestAndSet ("key",None, Some "X")
   ] in
-  let b = Buffer.create 1024 in
-  let () = Update.to_buffer b s in
-  let flat = Buffer.contents b in
-  let s',_ = Update.from_buffer flat 0 in
-  Printf.printf "got:%s\n%!" (Update.string_of s');
-  OUnit.assert_equal s s'
+  let s' = _b2b s in
+  _cmp s s'
+
+let test_range() = 
+  let r0 = Range.max in
+  let u0 = Update.SetRange r0 in
+  let u0' = _b2b u0 in
+  _cmp u0 u0';
+  let r1 = ((Some "b",Some "k"),(Some "a",Some "z")) in
+  let u1 = Update.SetRange r1 in
+  let u1' = _b2b u1 in
+  _cmp u1 u1'
 
 let suite = "update" >:::[
   "sequence" >:: test_sequence;
+  "range" >:: test_range;
 ]
