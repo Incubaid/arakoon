f3ac2b5a01f667bd7aef3ffeaa11ec64ea4852a0
Romain Slootmaekers <rslootma@b-virtual.org>
2011-07-12 16:47:37 +0200

    right:more imports(5)

--- extension/test/server/right/system_tests_long_right.py
+++ extension/test/server/right/system_tests_long_right.py
@@ -28,6 +28,7 @@ from ..system_tests_common import basic_teardown
 from ..system_tests_common import default_setup
 from ..system_tests_common import setup_2_nodes
 from ..system_tests_common import setup_3_nodes
+from ..system_tests_common import setup_1_node_forced_master
 from ..system_tests_common import setup_2_nodes_forced_master
 from ..system_tests_common import setup_3_nodes_forced_master
 from ..system_tests_common import node_names
