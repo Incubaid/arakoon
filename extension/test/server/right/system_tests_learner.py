6f35a18e06d84851c44fd9e46f4b97ef5821e566
Romain Slootmaekers <rslootma@b-virtual.org>
2011-07-13 12:52:09 +0200

    system_tests_learner

--- extension/test/server/right/system_tests_learner.py
+++ extension/test/server/right/system_tests_learner.py
@@ -28,20 +28,20 @@ import logging
 @Common.with_custom_setup(Common.setup_2_nodes, Common.basic_teardown)
 def test_learner():
     op_count = 54321
-    Common.iterate_n_times(op_count, simple_set)
-    cluster = q.manage.arakoon.getCluster(cluster_id)
+    Common.iterate_n_times(op_count, Common.simple_set)
+    cluster = Common.q.manage.arakoon.getCluster(Common.cluster_id)
     logging.info("adding learner")
     name = Common.node_names[2]
     (db_dir, log_dir) = Common.build_node_dir_names(name)
     cluster.addNode(name,
-                    node_ips[2],
-                    clientPort = node_client_base_port + 2,
-                    messagingPort = node_msg_base_port + 2,
+                    Common.node_ips[2],
+                    clientPort = Common.node_client_base_port + 2,
+                    messagingPort = Common.node_msg_base_port + 2,
                     logDir = log_dir,
                     logLevel = 'debug',
                     home = db_dir,
                     isLearner = True,
-                    targets = [node_names[0]])
+                    targets = [Common.node_names[0]])
 
     cluster.addLocalNode(name)
     cluster.createDirs(name)
@@ -51,7 +51,7 @@ def test_learner():
     time.sleep(op_count / 1000 + 1 ) # 1000/s in catchup should be no problem
     #use a client ??"
     Common.stop_all()
-    i2 = int(get_last_i_tlog(name))
+    i2 = int(Common.get_last_i_tlog(name))
     assert_true(i2 >= op_count - 1)
 
     
