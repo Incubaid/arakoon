02c50e5a60bf805b1daa1a995ecac010c9bb42d0
Romain Slootmaekers <rslootma@b-virtual.org>
2011-07-18 11:08:28 +0200

    renabling tlc tests

--- src/all_test.ml
+++ src/all_test.ml
@@ -50,9 +50,9 @@ let paxos_tests = "paxos" >::: [Multi_paxos_test.suite;]
 
 let update_tests = "updates" >::: [Update_test.suite]
 
-let tlog_tests = "tlogs" >::: [(*Tlog_test.suite;
+let tlog_tests = "tlogs" >::: [Tlog_test.suite;
 			       Tlogcollection_test.suite_mem;
-			       Tlc2_test.suite;*)
+			       Tlc2_test.suite;
 			      ]
 let small_catchup = "small_catchup" >:::[(* Catchup_test.suite; *)]
 let big_catchup = "big_catchup" >::: [(* Compression_test.suite;*)]
