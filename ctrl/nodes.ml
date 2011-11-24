9bf812913ab2832faad3385dcca7df0cb4c5d385
toolslive <romain@incubaid.com>
2011-11-24 11:05:29 +0100

    better default handling in config (if dir is not defined, it's the home dir); Lwt-2.3.2

--- ctrl/nodes.ml
+++ ctrl/nodes.ml
@@ -5,6 +5,7 @@ module Node = struct
   let names = ["arakoon_0";"arakoon_1";"arakoon_2"] 
   let cfg_name = (Unix.getcwd()) ^ "/cfg/arakoon.ini" 
 
+  let root = "/tmp/arakoon"
   let _node_cmd fmt name = 
     let cmd = Printf.sprintf fmt exec name cfg_name in
     print_endline cmd; 
@@ -22,8 +23,8 @@ module Node = struct
 
   let monitor_all () =
     let tab x = Printf.sprintf 
-      "--tab-with-profile=Default -e 'tail -f /tmp/%s/%s.log' -t '%s' " 
-      x x x 
+      "--tab-with-profile=Default -e 'tail -f %s/%s/%s.log' -t '%s' " 
+      root x x x 
     in
     let tabs = List.map tab names in
     let cmd = List.fold_left (^) "gnome-terminal " tabs in
@@ -32,11 +33,14 @@ module Node = struct
 
   let mkdirs () =
     let mkdir x = 
-      let dir_name = Printf.sprintf "/tmp/%s" x in
+      let dir_name = Printf.sprintf "%s/%s" root x in
 	Unix.mkdir dir_name 0o755
     in List.map mkdir names
 
   let clean_dirs () = 
-    List.iter (fun name -> ignore (Sys.command ("rm -rf /tmp/" ^ name))) names
+    let dir_name name = Printf.sprintf "%s/%s" root name in
+    List.iter 
+      (fun name -> ignore (Sys.command ("rm -rf " ^ (dir_name name)))) 
+      names
  
 end
