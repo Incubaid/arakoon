3d2370016e4fedc82078513c6f9577dab1a4ea62
Nicolas Trangez <ikke@nicolast.be>
2011-12-21 15:39:03 +0100

    Add ReStructuredText input support to build process
    Whenever a file with the extension '.rst' is found in the src/ folder,
    it will be preprocessed by Docutils into an HTML snippet, which will be
    used as part of a default mini-template.

--- src/build.py
+++ src/build.py
@@ -2,15 +2,66 @@ import os
 import os.path
 import logging
 
+import xml.xpath
+import xml.dom.minidom
+
 import jinja2
+
+import docutils
+import docutils.core
 
 import feedparser
 
 URI = 'http://blog.incubaid.com/category/arakoon/feed/'
 ENCODING = 'utf-8'
-FILTER = lambda f: f.endswith('.html') and not f.startswith('_')
+FILTER = lambda f: \
+    (f.endswith('.html') or f.endswith('.rst')) \
+    and not (f.startswith('_'))
+
+RST_SETTINGS = {
+    'initial_header_level': 2,
+}
+RST_TEMPLATE = u'''
+{%% extends '_base.html' %%}
+{%% block title %%}%(title)s{%% endblock %%}
+{%% block main %%}
+%(main)s
+{%% endblock %%}
+'''
 
 LOGGER = logging.getLogger(__name__)
+
+def render_rst(in_, out):
+    fd = open(in_, 'r') # Will be closed by docutils
+
+    text, doc = docutils.core.publish_programmatically(
+        source_class=docutils.io.FileInput, source=fd, source_path=in_,
+        destination_class=docutils.io.StringOutput, destination=None,
+        destination_path=None,
+        reader=None, reader_name='standalone',
+        parser=None, parser_name='restructuredtext',
+        writer=None, writer_name='html',
+        settings=None, settings_spec=None, settings_overrides=RST_SETTINGS,
+        config_section=None, enable_exit_status=False)
+
+    title = doc.document['title']
+
+    body = xml.xpath.Evaluate(
+        '/html/body/div[@class=\'document\']',
+        xml.dom.minidom.parseString(text).documentElement)[0]
+    body_html = body.toxml()
+
+    template = RST_TEMPLATE % {
+        'title': title,
+        'main': body_html,
+    }
+
+    data = template.encode('utf-8')
+    fd_out = open(out, 'w')
+    try:
+        fd_out.write(data)
+    finally:
+        fd_out.close()
 
 def run(src, target):
     LOGGER.info('Rendering %s to %s', src, target)
@@ -27,22 +78,41 @@ def run(src, target):
     for name in filter(FILTER, os.listdir(src)):
         LOGGER.debug('Rendering %s', name)
 
-        template = environment.get_template(name)
+        cleanup_html = False
+        html_file = None
 
-        local_context = context.copy()
-        local_context['name'] = os.path.splitext(name)[0]
+        template_name = name
 
-        output = template.render(local_context)
-        output_str = output.encode(ENCODING)
+        if name.endswith('.rst'):
+            cleanup_html = True
 
-        out = os.path.join(target, name)
-        fd = open(out, 'w')
+            basename = name[:-4]
+            template_name = '%s.html' % basename
+            html_file = os.path.join(src, template_name)
+            assert not os.path.exists(html_file)
+
+            render_rst(os.path.join(src, name), html_file)
+
         try:
-            LOGGER.debug('Writing to %s', out)
+            template = environment.get_template(template_name)
 
-            fd.write(output_str)
+            local_context = context.copy()
+            local_context['name'] = os.path.splitext(template_name)[0]
+
+            output = template.render(local_context)
+            output_str = output.encode(ENCODING)
+
+            out = os.path.join(target, template_name)
+            fd = open(out, 'w')
+            try:
+                LOGGER.debug('Writing to %s', out)
+
+                fd.write(output_str)
+            finally:
+                fd.close()
         finally:
-            fd.close()
+            if cleanup_html:
+                os.unlink(html_file)
 
 def main():
     src = os.path.abspath(os.path.dirname(__file__))
