222b58d7887dd61dd314d6ea0decd49d9f3b429a
Romain Slootmaekers <romain@incubaid.com>
2012-01-16 19:19:21 +0100

    hudson_packages should die on error

--- hudson_packages.sh
+++ hudson_packages.sh
@@ -1,8 +1,13 @@
 export BRANCH=$(hg branch)
-export ARAKOON_BUILD_ENV="/opt/arakoon_build_end/${BRANCH}"
-export PATH=${ARAKOON_BUILD_ENV}/OCAML/bin:${PATH}
+export ARAKOON_BUILD_ENV="/opt/arakoon_build_env/default"
+export OCAML_HOME=${ARAKOON_BUILD_ENV}/OCAML
+export LIBRARY_PATH=${OCAML_HOME}/lib:${LIBRARY_PATH}
+export LD_LIBRARY_PATH=${OCAML_HOME}/lib:${LD_LIBRARY_PATH}
+export PATH=${OCAML_HOME}/bin:${PATH}
 export DEB_BUILD_OPTIONS=nocheck
 
+set -e
+which ocaml
 fakeroot debian/rules clean
 fakeroot debian/rules build
 fakeroot debian/rules binary
@@ -15,4 +20,5 @@ mv ../arakoon_1.0.0-1_amd64.deb ${ARTIFACTS}
 python setup.py bdist_egg
 
 mv dist/arakoon-${BRANCH}-py2.7.egg ${ARTIFACTS}
+
 
