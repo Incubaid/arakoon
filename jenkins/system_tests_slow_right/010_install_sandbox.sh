f0e73e7dd6105e4477179318ef3d81c4bea90656
Romain Slootmaekers <romain@incubaid.com>
2012-01-20 16:35:59 +0100

    make left & right 010_install_sandbox.sh identical

--- jenkins/system_tests_slow_right/010_install_sandbox.sh
+++ jenkins/system_tests_slow_right/010_install_sandbox.sh
@@ -1,8 +1,4 @@
-whoami
-sudo -l
-echo $PATH
-which rm
 sudo rm -rf /opt/qbase3
-sudo tar xfz /home/incubaid/qbaseSandbox_3.2-small-linux64.tar.gz -C /opt
-sudo cp /home/incubaid/sources/publish.cfg /opt/qbase3/cfg/qpackages4/sources.cfg
-sudo cp /home/incubaid/hgconnection.cfg /opt/qbase3/cfg/qconfig/hgconnection.cfg
+sudo tar xfz /home/qbase/qbaseSandbox_3.2-small-linux64.tar.gz -C /opt
+sudo cp /home/qbase/sources/publish.cfg /opt/qbase3/cfg/qpackages4/sources.cfg
+sudo cp /home/qbase/hgconnection.cfg /opt/qbase3/cfg/qconfig/hgconnection.cfg
