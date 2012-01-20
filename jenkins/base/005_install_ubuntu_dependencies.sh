e2f5f360a595a091c4bacff55c30b7cad085f9c1
Nicolas Trangez <ikke@nicolast.be>
2012-01-20 15:14:02 +0100

    CI: Create virtualenv in doc build

--- jenkins/base/005_install_ubuntu_dependencies.sh
+++ jenkins/base/005_install_ubuntu_dependencies.sh
@@ -2,6 +2,6 @@
 
 sudo aptitude update || true
 
-for PKG in fakeroot python-epydoc python-setuptools debhelper dh-ocaml; do
+for PKG in fakeroot python-epydoc python-setuptools debhelper dh-ocaml python-virtualenv; do
     sudo aptitude install -yVDq $PKG
 done
--- jenkins/base/020_build_docs.sh
+++ jenkins/base/020_build_docs.sh
@@ -1,6 +1,17 @@
-#!/bin/bash -e
+#!/bin/bash -ex
 
-source /opt/arakoon_doc_env/bin/activate
+$VENV=_virtualenv
+
+if [ ! -d $VENV ]; then
+        virtualenv --no-site-packages $VENV
+        source $VENV/bin/activate
+
+        pip install sphinx
+
+        deactivate
+fi
+
+source $VENV/bin/activate
 
 cd doc
 make html
