6008a4ec9fb10c881eca25be33e96b7aabb44177
Romain Slootmaekers <romain@incubaid.com>
2012-01-20 08:55:22 +0100

    eol in 010_install_sandbox.sh

--- jenkins/system_tests_slow_right/010_install_sandbox.sh
+++ jenkins/system_tests_slow_right/010_install_sandbox.sh
@@ -4,6 +4,5 @@ echo $PATH
 which rm
 sudo rm -rf /opt/qbase3
 sudo tar xfz /home/incubaid/qbaseSandbox_3.2-small-linux64.tar.gz -C /opt
-sudo cp /home/incubaid/sources/publish.cfg
-/opt/qbase3/cfg/qpackages4/sources.cfg
+sudo cp /home/incubaid/sources/publish.cfg /opt/qbase3/cfg/qpackages4/sources.cfg
 sudo cp /home/incubaid/hgconnection.cfg /opt/qbase3/cfg/qconfig/hgconnection.cfg
