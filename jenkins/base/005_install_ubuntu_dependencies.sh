23696b14cbefbff6358c26f8e74a8789f5ffd5b2
Nicolas Trangez <ikke@nicolast.be>
2012-01-21 03:39:56 +0100

    CI: Add "github-commit" Ruby dependencies for "base" job

--- jenkins/base/005_install_ubuntu_dependencies.sh
+++ jenkins/base/005_install_ubuntu_dependencies.sh
@@ -2,6 +2,8 @@
 
 sudo aptitude update || true
 
-for PKG in texlive texlive-latex-extra darcs git automake fakeroot python-epydoc python-setuptools debhelper dh-ocaml python-virtualenv graphviz; do
+for PKG in texlive texlive-latex-extra darcs git automake fakeroot python-epydoc python-setuptools debhelper dh-ocaml python-virtualenv graphviz ruby1.9.1 rubygems1.9.1 ruby1.9.1-dev libxml2-dev libxslt-dev; do
     sudo aptitude install -yVDq $PKG
 done
+
+gem1.9.1 install --user-install github-upload
