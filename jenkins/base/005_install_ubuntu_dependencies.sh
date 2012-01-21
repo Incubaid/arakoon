f9a7929d178369614c5c7b34a7c6d94c212ee4a9
Nicolas Trangez <ikke@nicolast.be>
2012-01-21 02:45:39 +0100

    CI: Add "graphviz" dependency for "base" job

--- jenkins/base/005_install_ubuntu_dependencies.sh
+++ jenkins/base/005_install_ubuntu_dependencies.sh
@@ -2,6 +2,6 @@
 
 sudo aptitude update || true
 
-for PKG in texlive texlive-latex-extra darcs git automake fakeroot python-epydoc python-setuptools debhelper dh-ocaml python-virtualenv; do
+for PKG in texlive texlive-latex-extra darcs git automake fakeroot python-epydoc python-setuptools debhelper dh-ocaml python-virtualenv graphviz; do
     sudo aptitude install -yVDq $PKG
 done
