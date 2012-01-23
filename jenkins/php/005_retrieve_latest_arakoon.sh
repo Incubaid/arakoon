7c1c40dc4fb69c1b986edec88ed1398cabbcdc01
Nicolas Trangez <ikke@nicolast.be>
2012-01-23 12:10:46 +0100

    CI: Disable Arakoon binary retrieval (for now)

--- jenkins/php/005_retrieve_latest_arakoon.sh
+++ jenkins/php/005_retrieve_latest_arakoon.sh
@@ -3,6 +3,8 @@
 LATEST_ARAKOON_URI=$JENKINS_URL/view/arakoon-1.0/job/arakoon-1.0-base/lastSuccessfulBuild/artifact/arakoon
 TEST_BINARY=src/client/php/test/arakoon
 
-test -f $TEST_BINARY && rm $TEST_BINARY
-wget -O $TEST_BINARY $LATEST_ARAKOON_URI
+# Fails due to Jenkins auth...
+#test -f $TEST_BINARY && rm $TEST_BINARY
+#wget -O $TEST_BINARY $LATEST_ARAKOON_URI
+
 chmod a+x $TEST_BINARY
