ab7bec44e97af58594da3cbdc28af6ebcc8b640a
toolslive <romain@incubaid.com>
2012-03-02 17:36:43 +0100

    probe jenkins

--- jenkins/php/010_run_tests.sh
+++ jenkins/php/010_run_tests.sh
@@ -5,9 +5,9 @@ USER=`whoami`
 cd src/client/php/test
 
 export PATH=/opt/qbase3/bin/:/opt/qbase3/apps/arakoon/bin:$PATH
-export LD_LIBRARY_PATH=/opt/qbase3/lib:/opt/qbase3/lib64
+export LD_LIBRARY_PATH=/lib/x86_64-linux-gnu:/opt/qbase3/lib64
 
-sudo php all_nodes_down_test.php
+sudo echo $LD_LIBRARY_PATH && php all_nodes_down_test.php
 sudo php all_nodes_up_test.php
 sudo php invalid_client_configuration_test.php
 sudo php master_failover_test.php
