00c8826ab5d25e64d8c6bf32063cfc3c59f1a2bf
toolslive <romain@incubaid.com>
2012-03-05 09:35:20 +0100

    disable step

--- jenkins/php/005_retrieve_latest_arakoon.sh
+++ jenkins/php/005_retrieve_latest_arakoon.sh
@@ -1,10 +1,12 @@
 #!/bin/bash -xue
 
-LATEST_ARAKOON_URI=$JENKINS_URL/view/arakoon-1.0/job/arakoon-1.0-base/lastSuccessfulBuild/artifact/arakoon
-TEST_BINARY=src/client/php/test/arakoon
+#What to do here? sandbox arakoon or not ???
+
+#LATEST_ARAKOON_URI=$JENKINS_URL/view/arakoon-1.0/job/arakoon-1.0-base/lastSuccessfulBuild/artifact/arakoon
+#TEST_BINARY=/opt/qbase3/apps/arakoon/bin/arakoon
 
 # Fails due to Jenkins auth...
 #test -f $TEST_BINARY && rm $TEST_BINARY
 #wget -O $TEST_BINARY $LATEST_ARAKOON_URI
 
-chmod a+x $TEST_BINARY
+#chmod a+x $TEST_BINARY
