cc99796ab379534ab23537bd6b3543f5d1e2eaab
toolslive <romain@incubaid.com>
2012-03-05 10:34:13 +0100

    auth. for other artifacts

--- jenkins/php/005_retrieve_latest_arakoon.sh
+++ jenkins/php/005_retrieve_latest_arakoon.sh
@@ -4,8 +4,9 @@
 LATEST_ARAKOON_URI=$JENKINS_URL/view/arakoon-1.2/job/arakoon-1.2-base/lastSuccessfulBuild/artifact/arakoon
 TEST_BINARY=./src/client/php/test/arakoon
 
+echo JENKINS_USER=${JENKINS_USER}
 # Fails due to Jenkins auth...
 test -f $TEST_BINARY && rm $TEST_BINARY
-wget -O $TEST_BINARY $LATEST_ARAKOON_URI
+wget -O $TEST_BINARY $LATEST_ARAKOON_URI --user=${JENKINS_USER} --password=${JENKINS_PASSWORD} 
 
 chmod a+x $TEST_BINARY
