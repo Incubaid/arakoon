a6599416a37907a579f65eee0e26bd26e78f1815
toolslive <romain@incubaid.com>
2012-03-05 10:49:52 +0100

    disable fetch of latest_arakoon

--- jenkins/php/005_retrieve_latest_arakoon.sh
+++ jenkins/php/005_retrieve_latest_arakoon.sh
@@ -6,7 +6,7 @@ TEST_BINARY=./src/client/php/test/arakoon
 
 echo JENKINS_USER=${JENKINS_USER}
 # Fails due to Jenkins auth...
-test -f $TEST_BINARY && rm $TEST_BINARY
-wget -O $TEST_BINARY $LATEST_ARAKOON_URI --user=${JENKINS_USER} --password=${JENKINS_PASSWORD} 
+#test -f $TEST_BINARY && rm $TEST_BINARY
+#wget -O $TEST_BINARY $LATEST_ARAKOON_URI --user=${JENKINS_USER} --password=${JENKINS_PASSWORD} 
 
-chmod a+x $TEST_BINARY
+#chmod a+x $TEST_BINARY
