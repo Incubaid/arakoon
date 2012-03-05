a465ff711f8c951fc2147ef5a8c65b481757204e
toolslive <romain@incubaid.com>
2012-03-05 10:53:40 +0100

    LD_LIBRARY_PATH prepend /lib

--- jenkins/php/010_run_tests.sh
+++ jenkins/php/010_run_tests.sh
@@ -5,7 +5,7 @@ USER=`whoami`
 cd src/client/php/test
 
 export PATH=/opt/qbase3/bin/:/opt/qbase3/apps/arakoon/bin:$PATH
-export LD_LIBRARY_PATH=/lib/x86_64-linux-gnu:/opt/qbase3/lib64
+export LD_LIBRARY_PATH=/lib:/lib/x86_64-linux-gnu:/opt/qbase3/lib64
 
 echo $LD_LIBRARY_PATH && php all_nodes_down_test.php
 #sudo php all_nodes_up_test.php
