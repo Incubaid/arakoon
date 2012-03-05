cab0fbb08085a3e6da65997fc3d9f82600e9795b
toolslive <romain@incubaid.com>
2012-03-05 11:05:59 +0100

    re-enable more tests

--- jenkins/php/010_run_tests.sh
+++ jenkins/php/010_run_tests.sh
@@ -7,8 +7,8 @@ cd src/client/php/test
 export PATH=/opt/qbase3/bin/:/opt/qbase3/apps/arakoon/bin:$PATH
 export LD_LIBRARY_PATH=/lib:/lib/x86_64-linux-gnu:/opt/qbase3/lib64
 
-echo $LD_LIBRARY_PATH && php all_nodes_down_test.php
-#sudo php all_nodes_up_test.php
+php all_nodes_down_test.php
+php all_nodes_up_test.php
 #sudo php invalid_client_configuration_test.php
 #sudo php master_failover_test.php
 #sudo php one_node_down_test.php
