4eed63e41e2097cf17ff4adfbb1e5156c4b29803
toolslive <romain@incubaid.com>
2012-03-06 15:38:26 +0100

    changing jenkins jobs to reflect current branch

---
+++ jenkins/archive_version.sh
@@ -0,0 +1,1 @@
+sudo /opt/qbase3/var/tmp/arakoon-x/arakoon.native --version > arakoonqpackage.version.txt
--- jenkins/prepareSystemTests.py
+++ jenkins/prepareSystemTests.py
@@ -9,7 +9,8 @@ for (p,v) in packages:
     pkg = i.qp.find(p,version = v)
     pkg.install()
 
-coDir = "/".join( [q.dirs.tmpDir, "arakoon-1.0"] )
+branch = "1.1"
+coDir = "/".join( [q.dirs.tmpDir, "arakoon-x"] )
 q.system.fs.createDir( coDir )
 q.system.process.run( "hg clone https://bitbucket.org/despiegk/arakoon %s" % coDir ) 
-
+q.system.process.run( "hg update %s" % branch, cwd = coDir)
