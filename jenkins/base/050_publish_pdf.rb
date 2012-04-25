e4c495db0cfb2921f3538507a9e060969eaea339
Nicolas Trangez <ikke@nicolast.be>
2012-04-25 15:03:28 +0200

    Jenkins: Put PDF upload to Github in begin/resque block

--- jenkins/base/050_publish_pdf.rb
+++ jenkins/base/050_publish_pdf.rb
@@ -6,15 +6,20 @@ require 'net/github-upload'
 login = `git config github.user`.chomp
 token = `git config github.token`.chomp
 
-gh = Net::GitHub::Upload.new(
-    :login => login,
-    :token => token
-)
+begin
+    gh = Net::GitHub::Upload.new(
+        :login => login,
+        :token => token
+    )
 
-direct_link = gh.replace(
-    :repos => "Incubaid/arakoon",
-    :file => `pwd`.chomp + "/doc/_build/latex/Arakoon.pdf",
-    :name => "arakoon-default.pdf",
-    :content_type => "application/pdf",
-    :description => "Arakoon documentation from 'default' branch"
-)
+    direct_link = gh.replace(
+        :repos => "Incubaid/arakoon",
+        :file => `pwd`.chomp + "/doc/_build/latex/Arakoon.pdf",
+        :name => "arakoon-default.pdf",
+        :content_type => "application/pdf",
+        :description => "Arakoon documentation from 'default' branch"
+    )
+rescue => e
+    puts "Upload failed"
+    puts e.message
+end
