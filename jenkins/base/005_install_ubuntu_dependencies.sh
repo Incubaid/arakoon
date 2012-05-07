afd035742b32f6039ee0ca17067e2e10089c121a
Kurt Bonne <kb@kb-laptop.(none)>
2012-05-07 10:55:04 +0200

    Updated jenkins scripts to use make/test_it.py

--- jenkins/base/005_install_ubuntu_dependencies.sh
+++ jenkins/base/005_install_ubuntu_dependencies.sh
@@ -2,7 +2,7 @@
 
 sudo aptitude update || true
 
-for PKG in texlive texlive-latex-extra darcs git automake fakeroot python-epydoc python-setuptools debhelper dh-ocaml python-virtualenv graphviz ruby1.9.1 rubygems1.9.1 ruby1.9.1-dev libxml2-dev libxslt-dev; do
+for PKG in texlive texlive-latex-extra darcs git automake fakeroot python-epydoc python-setuptools debhelper dh-ocaml python-virtualenv graphviz ruby1.9.1 rubygems1.9.1 ruby1.9.1-dev libxml2-dev libxslt-dev python-nose; do
     sudo aptitude install -yVDq $PKG
 done
 
--- jenkins/base/010_build.sh
+++ jenkins/base/010_build.sh
@@ -7,19 +7,16 @@ export PATH=$BUILD_ENV/bin:$PATH
 export LD_LIBRARY_PATH=$BUILD_ENV/lib
 export LIBRARY_PATH=$BUILD_ENV/lib
 
-ocamlfind list
-ocamlbuild -clean
-ocamlbuild -use-ocamlfind arakoon.native arakoon.byte
-#make coverage 
-#./arakoon.d.byte --run-all-tests-xml foobar.xml
-./arakoon.native --run-all-tests-xml foobar.xml
-./report.sh
+make clean
+make
+
+python test_it.py -s test.server.quick.system_tests_basic
 # redo this for artifacts...
-ocamlbuild -use-ocamlfind arakoon.native arakoon.byte
-mkdir -p doc/python/client
-epydoc --html --output ./doc/python/client --name Arakoon --url http://www.arakoon.org --inheritance listed --graph all src/client/python
-mkdir -p doc/python/client/extension
-epydoc --html --output ./doc/python/client/extension --name "Arakoon PyLabs client extension" --url http://www.arakoon.org --inheritance listed --graph all extension/client/*.py
-mkdir -p doc/python/server/extension
-epydoc --html --output ./doc/python/server/extension --name "Arakoon PyLabs server extension" --url http://www.arakoon.org --inheritance listed --graph all extension/server/*.py
-cp arakoon.native arakoon
+#ocamlbuild -use-ocamlfind barakoon.native barakoon.byte
+#mkdir -p doc/python/client
+#epydoc --html --output ./doc/python/client --name Arakoon --url http://www.arakoon.org --inheritance listed --graph all src/client/python
+#mkdir -p doc/python/client/extension
+#epydoc --html --output ./doc/python/client/extension --name "Arakoon PyLabs client extension" --url http://www.arakoon.org --inheritance listed --graph all extension/client/*.py
+#mkdir -p doc/python/server/extension
+#epydoc --html --output ./doc/python/server/extension --name "Arakoon PyLabs server extension" --url http://www.arakoon.org --inheritance listed --graph all extension/server/*.py
+#cp arakoon.native arakoon
