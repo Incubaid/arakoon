1b06eaf3650afd8894c51c513a5067894b5b3198
Romain Slootmaekers <romain@incubaid.com>
2012-05-09 10:49:59 +0200

    failing test ;)

--- src/hope/bstore_test.ml
+++ src/hope/bstore_test.ml
@@ -1,24 +1,19 @@
 open OUnit
 open Lwt
 open Bstore
+open Core
 
 let t_log () =  
   let main_t() = 
     let fn = "t_log.bs" in
     BStore.init fn >>= fun () ->
     BStore.create fn >>= fun t ->
-    let u = Core.SET("k","v") in
+    let u = SET("x","x") in
     BStore.log t true u >>= fun _ ->
+    let u2 = SEQUENCE [ASSERT("x",Some "x")] in
+    BStore.log t true u2 >>= fun _ ->
     Lwtc.failfmt "xxx" >>= fun () ->
     Lwt.return ()
-(*
-  client = C.get_client()
-  client.set('x','x')
-  ass = arakoon.ArakoonProtocol.Assert('x','x')
-  seq = arakoon.ArakoonProtocol.Sequence()
-  seq.addUpdate(ass)
-  client.sequence(seq)
-*)
   in
   Lwt_main.run (main_t())
 
