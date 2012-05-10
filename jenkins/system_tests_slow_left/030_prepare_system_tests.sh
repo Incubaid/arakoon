f86ec46ed773a5aab56e2f9ece4b9e067f8c0dad
Romain Slootmaekers <romain@incubaid.com>
2012-05-10 14:43:26 +0200

    left

--- jenkins/system_tests_slow_left/030_prepare_system_tests.sh
+++
@@ -1,3 +0,0 @@
-sudo /opt/qbase3/qshell ./jenkins/prepareSystemTests.py
-
-sudo mkdir -p ${WORKSPACE}/testresults
--- jenkins/system_tests_slow_left/080_archive_version.sh
+++
@@ -1,1 +0,0 @@
-sudo /opt/qbase3/var/tmp/arakoon-1.0/arakoon.native --version > arakoonqpackage.version.txt
