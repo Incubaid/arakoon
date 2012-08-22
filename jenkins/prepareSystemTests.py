e7cfbf6d3eeec58ec1f2d50fb6ea7a15758428d3
toolslive <romain@incubaid.com>
2012-08-22 17:41:22 +0200

    more verbose prepareSystemTests

--- jenkins/prepareSystemTests.py
+++ jenkins/prepareSystemTests.py
@@ -15,12 +15,19 @@ for (k,v) in os.environ.items():
     print k
 
 
-v = subprocess.Popen(['hg','branch'], stdout = subprocess.PIPE).communicate()[0]
+def run_it(cmd):
+    r = subprocess.Popen(cmd, stdout = subprocess.PIPE).communicate()[0]
+    return r
+
+
+which_hg = run_it(['which','hg'])
+hg_version = run_it(['hg', 'version'])
+v = run_it(['hg','branch'])
 branch = v.strip()
 
 coDir = "/".join( [q.dirs.tmpDir, "arakoon-x"] )
 
-print "branch=%s, coDir=%s" % (branch,coDir) 
+print "which_hg=%s\nhg_version=%s\nbranch=%s\ncoDir=%s" % (which_hg, hg_version, branch, coDir) 
 
 q.system.fs.createDir( coDir )
 q.system.process.run( "hg clone https://bitbucket.org/despiegk/arakoon %s" % coDir ) 
