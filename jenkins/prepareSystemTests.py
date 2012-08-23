100d0b4c18b13a10ffc6897d2769a470dbbb5688
toolslive <romain@incubaid.com>
2012-08-23 10:16:05 +0200

    jenkins jobs:abort prepareSystemTests when things go wrong

--- jenkins/prepareSystemTests.py
+++ jenkins/prepareSystemTests.py
@@ -17,7 +17,12 @@ for (k,v) in os.environ.items():
 
 
 def run_it(cmd):
-    r = subprocess.Popen(cmd, stdout = subprocess.PIPE).communicate()[0]
+    p = subprocess.Popen(cmd, stdout = subprocess.PIPE)
+    r = p.communicate()[0]
+    rc = p.returncode
+    if rc != 0:
+        e = Exception("cmd: %s returned %i" % (cmd, rc))
+        raise e
     return r
 
 
@@ -31,5 +36,6 @@ coDir = "/".join( [q.dirs.tmpDir, "arakoon-x"] )
 print "which_hg=%s\nhg_version=%s\nbranch=%s\ncoDir=%s" % (which_hg, hg_version, branch, coDir) 
 
 q.system.fs.createDir( coDir )
-q.system.process.run( "hg clone https://bitbucket.org/despiegk/arakoon %s" % coDir ) 
-q.system.process.run( "hg update %s" % branch, cwd = coDir)
+
+run_it(["hg","clone","https://bitbucket.org/despiegk/arakoon", coDir]) 
+run_it(["hg","update", branch], cwd = coDir)
