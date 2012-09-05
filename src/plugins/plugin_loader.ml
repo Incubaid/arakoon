84dca57da9544bce6ae4efabc34b762f8980799f
Romain Slootmaekers <romain@incubaid.com>
2012-09-05 09:29:26 +0200

    plugin loader: fatal logging when things go wrong

--- src/plugins/plugin_loader.ml
+++ src/plugins/plugin_loader.ml
@@ -10,7 +10,14 @@ let load home pnames =
       let full = Filename.concat home pwe in
       let qual = Dynlink.adapt_filename full in
       Lwt_log.info_f "Plugin_loader: qualified as: %S" qual >>= fun () ->
-      Dynlink.loadfile_private qual;
+      (try
+         Dynlink.loadfile_private qual;
+         Lwt.return ()
+       with
+           (Dynlink.Error e) as x -> let msg = Dynlink.error_message e in
+                              Lwt_log.fatal_f "Plugin_loader: [%S] failed with %S" qual msg >>= fun () ->
+                              Lwt.fail x
+      ) >>= fun () ->
       _inner ps 
       in
   _inner pnames
