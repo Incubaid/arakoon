f00528668d0138aea856937fbc0a0ef3d8e0866c
toolslive <romain@incubaid.com>
2012-11-23 14:25:06 +0100

    CI:set path before calling opam

--- jenkins/base/010_build.sh
+++ jenkins/base/010_build.sh
@@ -3,8 +3,9 @@
 echo WORKSPACE=${WORKSPACE}
 BUILD_ENV=${WORKSPACE%${JOB_NAME}}
 echo BUILD_ENV=${BUILD_ENV}
+PATH=${BUILD_ENV}/ROOT/OCAML/bin:$PATH
 eval `${BUILD_ENV}/ROOT/OPAM/bin/opam --root ${BUILD_ENV}/ROOT/OPAM_ROOT config -env`
-PATH=$PATH:${BUILD_ENV}/ROOT/OCAML/bin
+
 
 ocamlfind printconf
 ocamlfind list | grep bz2
