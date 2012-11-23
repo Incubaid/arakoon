9c7e1439aa8cc3828a36a7fe16b9a4815e295fe5
toolslive <romain@incubaid.com>
2012-11-23 16:38:28 +0100

    CI:build_in_sandbox

--- jenkins/build_in_shandbox.sh
+++ jenkins/build_in_shandbox.sh
@@ -1,2 +1,17 @@
-export PATH=/opt/qbase3/bin:$PATH
+echo WORKSPACE=${WORKSPACE}
+BUILD_ENV=${WORKSPACE%${JOB_NAME}}
+echo BUILD_ENV=${BUILD_ENV}
+PATH=${BUILD_ENV}/ROOT/OCAML/bin:$PATH
+eval `${BUILD_ENV}/ROOT/OPAM/bin/opam --root ${BUILD_ENV}/ROOT/OPAM_ROOT config -env`
+
+
+ocamlfind printconf
+ocamlfind list | grep bz2
+ocamlfind list | grep lwt
+ocamlfind list | grep camltc
+
+ocamlbuild -clean
+ocamlbuild -use-ocamlfind arakoon.native arakoon.byte
+
 sudo ./buildInSandbox.sh
+
