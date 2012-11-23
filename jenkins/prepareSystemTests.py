182e28887a89e26f16f0854dfb28c4125a7addbc
toolslive <romain@incubaid.com>
2012-11-23 17:30:56 +0100

    CI: jenkins ...

---
+++ jenkins/prepareSystemTests.py
@@ -0,0 +1,12 @@
+from pymonkey.InitBase import i,q
+import os
+import subprocess
+
+packages = [
+            ('testrunner','1.1'),
+           ]
+
+for (p,v) in packages:
+    pkg = i.qp.find(p,version = v)
+    pkg.install()
+
--- jenkins/system_tests_quick/030_prepare_system_tests.sh
+++ jenkins/system_tests_quick/030_prepare_system_tests.sh
@@ -1,1 +1,2 @@
+sudo /opt/qbase3/qshell ./jenkins/prepareSystemTests.py
 sudo mkdir -p ${WORKSPACE}/testresults
