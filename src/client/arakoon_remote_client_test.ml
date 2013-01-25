94be12a6f6c04df99981e2792727c63186e4adce
Romain Slootmaekers <romain@incubaid.com>
2013-01-25 11:08:49 +0100

    set_get skeleton for arakoon_remote test

--- src/client/arakoon_remote_client_test.ml
+++ src/client/arakoon_remote_client_test.ml
@@ -49,6 +49,7 @@ let create_store _ myname =
 
 
 let __client_server_wrapper__ cluster (real_test:real_test) =
+  let () = Lwt_log.Section.set_level Lwt_log.Section.main Lwt_log.Debug in
   let port = 7777 in
   
   let conversation connection  =
@@ -60,32 +61,20 @@ let __client_server_wrapper__ cluster (real_test:real_test) =
   let sleep, notifier = wait () in
   let td_var = Lwt_mvar.create_empty () in
   let setup_callback () =
-    Lwt_log.info "callback" >>= fun () ->
+    Lwt_log.info "setup_callback" >>= fun () ->
     Lwt.wakeup notifier ();
     Lwt.return ()
   in
-
+  let me= "node_0" in
   let teardown_callback () = Lwt_mvar.put td_var () in
-  let me= "node0" in
+  let my_address = ("127.0.0.1", 7778) in
   let stats = Statistics.create () in
   let resyncs = create_resyncs [] _CLUSTER in
-  let server () = 
-    create_store () me >>= fun store ->
-    let timeout_q = PQ.create () in 
-    let drop_it _ _ _ = false in
-    let messaging = new Tcp_messaging.tcp_messaging 
-      ("127.0.0.1", 7778) _CLUSTER drop_it
-    in
-    let disp     = MyDispatcher.create messaging store timeout_q resyncs in
-    let driver   = MyDriver.create disp timeout_q (PQ.create()) in
-    let protocol = MyHandler.protocol me stats driver store in
-    Server.make_server_thread 
-      ~setup_callback 
-      ~teardown_callback
-      "127.0.0.1" port
-      protocol ()
-  in
-  
+  let timeout_q = PQ.create () in 
+  let drop_it _ _ _ = false in
+  let messaging = new Tcp_messaging.tcp_messaging my_address _CLUSTER drop_it in
+  let () = messaging # register_receivers [me, my_address] in
+
   let client_t () =
     let address = Unix.ADDR_INET (Unix.inet_addr_loopback, port) in
     Lwt_io.open_connection address >>= function (ic,oc) ->
@@ -93,30 +82,76 @@ let __client_server_wrapper__ cluster (real_test:real_test) =
       Lwt_log.debug "end_of_senario" >>= fun () ->
       Lwt_io.close ic >>= fun () ->
       Lwt_io.close oc >>= fun () ->
-    Lwt.return ()
+      Lwt.return ()
   in
-  
+  let lease_period = 3.0 in  
+  let build_start_state () = 
+    let q = 1 in
+    let n_others = [] in
+    let ix = 0 in
+    let node_cnt = 1 in
+    let c = Mp.MULTI.build_mp_constants q me n_others lease_period ix node_cnt in
+    let n = Core.start_tick in
+    let p = Core.start_tick in
+    let a = Core.start_tick in
+    let u = None in
+    Mp.MULTI.build_state c n p a u 
+  in
   let main =
+    create_store () me >>= fun store ->
+    let disp     = MyDispatcher.create messaging store timeout_q resyncs in
+    let q        = PQ.create () in
+    let driver   = MyDriver.create disp timeout_q q in
+    let protocol = MyHandler.protocol me stats driver store in
+    let s0 = build_start_state () in
+    let delayed_timeout = Mp.MULTI.A_START_TIMER (s0.Mp.MULTI.round, 
+                                                  Core.start_tick, 
+                                                  lease_period) 
+    in
+    let pass_msg msging q target = 
+      let rec loop () =  
+        msging # recv_message ~target >>= fun (msg_s, id) ->
+        let msg = Mp.MULTI.msg_of_string msg_s in
+        PQ.push q msg;
+        loop ()
+      in loop ()
+    in
+    MyDriver.dispatch driver s0 delayed_timeout >>= fun s1 ->
+    let server () = 
+      Server.make_server_thread 
+        ~setup_callback 
+        ~teardown_callback
+        "127.0.0.1" port
+        protocol ()
+    in
+
     Lwt.pick [
-      client_t (); 
+      messaging # run();
+      MyDriver.serve driver s1 None;
 	  server ();
+      pass_msg messaging timeout_q me;
+      begin Lwt_unix.sleep 10.0 >>= fun () ->client_t () end; 
     ] 
     >>= fun () -> 
     Lwt_mvar.take td_var  >>= fun () ->
     Lwt_log.info "server down"
   in
+
   Lwt_main.run main
 
 
-let test_ping () =
-  let real_test client = 
-    client # ping "test_ping" _CLUSTER >>= fun server_version ->
-    OUnit.assert_equal ~printer:(fun s -> s) server_version "test_backend.0.0.0";
+let test_set_get () =
+  let real_test (client: Arakoon_client.client) = 
+    let v = "X" in
+    client # set "x" v >>= fun () ->
+    client # get "x" >>= fun v2 ->
+    Lwtc.log "v = %S v2 = %S" v v2 >>= fun ()-> 
+    OUnit.assert_equal ~printer:(fun s -> s) v v2;
     Lwt.return ()
   in 
   __client_server_wrapper__ _CLUSTER real_test
 
   
 let suite = "remote_client" >::: [
-  "ping"      >:: test_ping;
+  "set_get"      >:: test_set_get;
 ]
