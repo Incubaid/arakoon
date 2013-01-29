bff6276501019dd7bdadc1c4b8d56fc958ecf020
Romain Slootmaekers <romain@incubaid.com>
2013-01-29 11:45:36 +0100

    CI: jenkins base case

--- jenkins/base/010_build.sh
+++ jenkins/base/010_build.sh
@@ -1,15 +1,14 @@
 echo WORKSPACE=${WORKSPACE}
 BUILD_ENV=${WORKSPACE%${JOB_NAME}}
 echo BUILD_ENV=${BUILD_ENV}
-eval `${BUILD_ENV}/ROOT/OPAM/bin/opam --root ${BUILD_ENV}/ROOT/OPAM_ROOT config -env`
-PATH=$PATH:${BUILD_ENV}/ROOT/OCAML/bin
+PATH=${BUILD_ENV}/ROOT/OCAML/bin:$PATH
+eval `${BUILD_ENV}/ROOT/OPAM/bin/opam config env -r ${BUILD_ENV}/ROOT/OPAM_ROOT`
+
 
 ocamlfind printconf
 ocamlfind list | grep bz2
 ocamlfind list | grep lwt
-ocamlfind list | grep camltc
 ocamlfind list | grep baardskeerder
-
 
 make clean
 make
