e456fbde35554848f18528f1ae1992cb1845fcd8
Nicolas Trangez <ikke@nicolast.be>
2013-03-04 16:13:43 +0100

    Jenkins: Enable coverage reporting
    See: 17734cecdd95ad8787b6ea7fb1bcd8ef04738279

--- jenkins/base/010_build.sh
+++ jenkins/base/010_build.sh
@@ -10,15 +10,18 @@ ocamlfind list | grep camltc
 ocamlbuild -clean
 ocamlbuild -use-ocamlfind arakoon.native arakoon.byte
 
+#./arakoon.d.byte --run-all-tests-xml foobar.xml
+./arakoon.native --run-all-tests-xml foobar.xml
+
+make coverage
+./report.sh
+
+# redo this for artifacts...
+ocamlbuild -use-ocamlfind arakoon.native arakoon.byte
+
 echo "Fixup symlinks (absolute to relative)"
 symlinks -c .
 
-#make coverage 
-#./arakoon.d.byte --run-all-tests-xml foobar.xml
-./arakoon.native --run-all-tests-xml foobar.xml
-./report.sh
-# redo this for artifacts...
-ocamlbuild -use-ocamlfind arakoon.native arakoon.byte
 mkdir -p doc/python/client
 epydoc --html --output ./doc/python/client --name Arakoon --url http://www.arakoon.org --inheritance listed --graph all src/client/python
 mkdir -p doc/python/client/extension
