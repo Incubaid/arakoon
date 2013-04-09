a3927bbac0aab69bee794549713ef792b9c73569
Romain Slootmaekers <romain@incubaid.com>
2013-04-09 14:56:06 +0200

    correct bad merge conflict resolution (from wip-ARAKOON-311)

--- src/client/client_protocol.ml
+++ src/client/client_protocol.ml
@@ -600,6 +600,7 @@ let one_command (ic,oc) (backend:Backend.backend) log_commands =
             (handle_exception oc)
         end
     | VERSION ->
+      begin
         log_command_f "VERSION" >>= fun () ->
         Llio.output_int oc 0 >>= fun () ->
         Llio.output_int oc Version.major >>= fun () ->
@@ -612,6 +613,15 @@ let one_command (ic,oc) (backend:Backend.backend) log_commands =
         in
         Llio.output_string oc rest >>= fun () ->
         Lwt.return false
+      end
+    | DROP_MASTER ->
+      begin
+        log_command_f "DROP_MASTER" >>= fun () ->
+        Lwt.catch
+          (fun () -> backend # drop_master () >>= fun () ->
+            response_ok_unit oc)
+          (handle_exception oc)
+      end
 
 let protocol backend log_commands connection =
   let ic,oc = connection in
