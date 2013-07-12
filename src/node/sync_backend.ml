Incubaid/arakoon

commit 2f2371e305f677a17c4c19e32b82f38fdc5b9369

only retry catchup connection on another ip if connection never successfully opened reading the last tlog entry (still being written) should fail with End_of_file not TLogCheckSumError

src/tlog/tlogcommon.ml
@@ -67,45 +67,28 @@ let validateTlogEntry buffer checkSum =
 
 let read_entry ic =
   let last_valid_pos = Lwt_io.position ic in
-  Lwt.catch 
-    ( fun () ->
-      Sn.input_sn    ic >>= fun  i     ->
-      Llio.input_int32 ic >>= fun chkSum ->
-      Llio.input_string ic >>= fun cmd  -> 
-      (* if you want to do validation, do it here *)
-      let cmdl = String.length cmd in
-      let chksum2 = Crc32c.calculate_crc32c cmd 0 cmdl in
-      begin
-        if chkSum <> chksum2 
-        then Lwt.fail (TLogCheckSumError last_valid_pos )
-        else Lwt.return ()
-      end >>= fun () ->
-      let value,off = Value.value_from cmd 0 in
-      let marker = 
-        if off = cmdl 
-        then None 
-        else 
-          let m,_ = Llio.string_option_from cmd off in
-          m
-      in
-      let (entry : Entry.t) = Entry.make i value last_valid_pos marker in
-      Lwt.return entry
-    ) 
-    ( function
-      | End_of_file ->
-        begin
-          let new_pos = Lwt_io.position ic in 
-          Logger.log_ Logger.Section.main Logger.Debug (fun () -> Printf.sprintf "Last valid pos: %d, new pos: %d" (Int64.to_int new_pos) 
-            (Int64.to_int last_valid_pos)) >>= fun () ->
-          begin 
-            if ( Int64.compare new_pos last_valid_pos ) = 0 
-            then Lwt.fail End_of_file
-            else Lwt.fail (TLogCheckSumError last_valid_pos)
-          end
-        end
-      | ex -> Lwt.fail ex
-    ) 
- 
+  Sn.input_sn    ic >>= fun  i     ->
+  Llio.input_int32 ic >>= fun chkSum ->
+  Llio.input_string ic >>= fun cmd  ->
+  (* if you want to do validation, do it here *)
+  let cmdl = String.length cmd in
+  let chksum2 = Crc32c.calculate_crc32c cmd 0 cmdl in
+  begin
+    if chkSum <> chksum2
+    then Lwt.fail (TLogCheckSumError last_valid_pos )
+    else Lwt.return ()
+  end >>= fun () ->
+  let value,off = Value.value_from cmd 0 in
+  let marker =
+    if off = cmdl
+    then None
+    else
+      let m,_ = Llio.string_option_from cmd off in
+      m
+  in
+  let (entry : Entry.t) = Entry.make i value last_valid_pos marker in
+  Lwt.return entry
+
 
 let entry_from buff pos = 
   let i, pos2  = Sn.sn_from       buff pos  in

src/node/catchup.ml
@@ -38,23 +38,34 @@ type store_tlc_cmp =
   | Store_tlc_equal
   | Store_ahead
 
-let _with_client_connection (ips,port) f = 
+let _with_client_connection (ips,port) f =
   let sl2s ss = list2s (fun s -> s) ss in
   let rec loop = function
     | [] -> Llio.lwt_failfmt "None of the ips: %s can be reached" (sl2s ips)
     | ip :: rest ->
-      Lwt.catch
-        (fun () ->
-          let address = Network.make_address ip port in
-          Lwt_io.with_connection address f
-        )
-        (fun exn -> 
-          Logger.info_f_ ~exn "ip = %s " ip >>= fun () ->
-          loop rest
-        )
+        Lwt.catch
+          (fun () ->
+            let address = Network.make_address ip port in
+            Lwt_io.with_connection address f)
+          (fun exn ->
+            Logger.info_f_ ~exn "ip = %s " ip >>= fun () ->
+            match exn with
+              | Unix.Unix_error(error,_,_) ->
+                  begin
+                    match error with
+                      | Unix.ECONNREFUSED
+                      | Unix.ENETUNREACH
+                      | Unix.EHOSTUNREACH
+                      | Unix.EHOSTDOWN ->
+                          loop rest
+                      | _ -> Lwt.fail exn
+                  end
+              | Lwt_unix.Timeout ->
+                  loop rest
+              | _ -> Lwt.fail exn)
   in
   loop ips
-      
+
 
 let compare_store_tlc store tlc =
   tlc # get_last_i () >>= fun tlc_i ->

commit c21b5e4408387e52e1f1486a2489ff2ab2d22138

throw TLogUnexcpectedEndOfFile when reaching the end of the tlog unexpectedly, and treat as a TLogCheckSumError in most places This fixes a bug in truncating introduced in 2f2371e305f677a17c4c19e32b82f38fdc5b9369

src/node/sync_backend.ml
@@ -384,7 +384,11 @@ object(self: #backend)
           and v = Entry.v_of entry
           in
           Tlogcommon.write_entry oc i v in
-	    tlog_collection # iterate start_i3 too_far_i f >>= fun () ->
+        Lwt.catch
+          (fun () -> tlog_collection # iterate start_i3 too_far_i f)
+          (function
+            | Tlogcommon.TLogUnexpectedEndOfFile _ -> Lwt.return ()
+            | ex -> Lwt.fail ex) >>= fun () ->
 	    Sn.output_sn oc (-1L)
 	  end
     end

src/tlog/tlc2.ml
@@ -216,7 +216,8 @@ let _validate_one tlog_name node_id ~check_marker : validation_result Lwt.t =
     )
     (function
       | Unix.Unix_error(Unix.ENOENT,_,_) -> Lwt.return (None, new_index)
-      | Tlogcommon.TLogCheckSumError pos ->
+      | Tlogcommon.TLogCheckSumError pos
+      | Tlogcommon.TLogUnexpectedEndOfFile pos ->
         begin
           match !prev_entry with
             | Some entry -> let i = Entry.i_of entry in Lwt.fail (TLCCorrupt (pos,i))
@@ -811,7 +812,8 @@ let truncate_tlog filename =
           Lwt.catch
             (fun() -> folder ic ~index lowerI higherI ~first () skipper)
             (function
-              | Tlogcommon.TLogCheckSumError pos ->
+              | Tlogcommon.TLogCheckSumError pos
+              | Tlogcommon.TLogUnexpectedEndOfFile pos ->
                 let fd = Unix.openfile filename [ Unix.O_RDWR ] 0o600 in
                 Lwt.return ( Unix.ftruncate fd (Int64.to_int pos) )
               | ex -> Lwt.fail ex

src/tlog/tlogcommon.ml
@@ -40,6 +40,7 @@ module Entry = struct
 end
 
 exception TLogCheckSumError of Int64.t
+exception TLogUnexpectedEndOfFile of Int64.t
 
 let tlogEntriesPerFile = 
   ref (IFDEF SMALLTLOG THEN 1000 ELSE (100 * 1000) END)
@@ -67,27 +68,41 @@ let validateTlogEntry buffer checkSum =
 
 let read_entry ic =
   let last_valid_pos = Lwt_io.position ic in
-  Sn.input_sn    ic >>= fun  i     ->
-  Llio.input_int32 ic >>= fun chkSum ->
-  Llio.input_string ic >>= fun cmd  ->
-  (* if you want to do validation, do it here *)
-  let cmdl = String.length cmd in
-  let chksum2 = Crc32c.calculate_crc32c cmd 0 cmdl in
-  begin
-    if chkSum <> chksum2
-    then Lwt.fail (TLogCheckSumError last_valid_pos )
-    else Lwt.return ()
-  end >>= fun () ->
-  let value,off = Value.value_from cmd 0 in
-  let marker =
-    if off = cmdl
-    then None
-    else
-      let m,_ = Llio.string_option_from cmd off in
-      m
-  in
-  let (entry : Entry.t) = Entry.make i value last_valid_pos marker in
-  Lwt.return entry
+  Lwt.catch
+    (fun () ->
+      Sn.input_sn    ic >>= fun  i     ->
+      Llio.input_int32 ic >>= fun chkSum ->
+      Llio.input_string ic >>= fun cmd  ->
+      let cmdl = String.length cmd in
+      let chksum2 = Crc32c.calculate_crc32c cmd 0 cmdl in
+      begin
+        if chkSum <> chksum2
+        then Lwt.fail (TLogCheckSumError last_valid_pos )
+        else Lwt.return ()
+      end >>= fun () ->
+      let value,off = Value.value_from cmd 0 in
+      let marker =
+        if off = cmdl
+        then None
+        else
+          let m,_ = Llio.string_option_from cmd off in
+          m
+      in
+      let (entry : Entry.t) = Entry.make i value last_valid_pos marker in
+      Lwt.return entry)
+    (function
+      | End_of_file ->
+          begin
+            let new_pos = Lwt_io.position ic in
+            Logger.log_ Logger.Section.main Logger.Debug (fun () -> Printf.sprintf "Last valid pos: %d, new pos: %d" (Int64.to_int new_pos)
+              (Int64.to_int last_valid_pos)) >>= fun () ->
+            begin
+              if ( Int64.compare new_pos last_valid_pos ) = 0
+              then Lwt.fail End_of_file
+              else Lwt.fail (TLogUnexpectedEndOfFile last_valid_pos)
+            end
+          end
+      | ex -> Lwt.fail ex)
 
 
 let entry_from buff pos = 