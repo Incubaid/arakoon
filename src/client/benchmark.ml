e6d0bcfae14799b11a4ce259adf74503c0c4b1f6
Romain Slootmaekers <romain@incubaid.com>
2013-08-22 17:52:26 +0200

    benchmark supports key size as well

--- src/client/benchmark.ml
+++ src/client/benchmark.ml
@@ -41,7 +41,8 @@ let _progress t0 n step (oc:Lwt_io.output_channel) =
   else
     Lwt.return()
 
-let _get (client:Arakoon_client.client) max_n t0 oc =
+let _get (client:Arakoon_client.client) max_n k_size t0 oc =
+  let k0 = String.make (k_size - 8 ) '_' in
   let rec loop n =
     if n = max_n
     then Lwt.return ()
@@ -49,15 +50,18 @@ let _get (client:Arakoon_client.client) max_n t0 oc =
       begin
         _progress t0 n 10000 oc >>= fun () ->
         let i = Random.int max_n in
-        let key = _cat "key" i
+        let key = _cat k0 i
         in client # get key >>= fun _ ->
         loop (n+1)
       end
   in
   loop 0
 
-let _get_transactions (client:Arakoon_client.client) max_n t size (t0:float) oc=
+let _get_transactions (client:Arakoon_client.client)
+    max_n t k_size v_size
+    (t0:float) oc=
   let n_transactions = (max_n + t -1) / t in
+  let k0 = String.make (k_size -8) '_' in
   let rec loop_t i =
     if i = n_transactions
     then Lwt.return ()
@@ -68,7 +72,7 @@ let _get_transactions (client:Arakoon_client.client) max_n t size (t0:float) oc=
           then acc
           else
             let i = Random.int max_n in
-            let key = _cat "key" i in
+            let key = _cat k0 i in
             build (key::acc) (j+1)
         in
         let keys = build [] 0 in
@@ -81,15 +85,16 @@ let _get_transactions (client:Arakoon_client.client) max_n t size (t0:float) oc=
 
 
 
-let _fill client max_n size (t0:float) (oc:Lwt_io.output_channel) =
-  let v0 = String.make (size - 8) 'x' in
+let _fill client max_n k_size v_size (t0:float) (oc:Lwt_io.output_channel) =
+  let k0 = String.make (k_size - 8) '_' in
+  let v0 = String.make (v_size - 8) 'x' in
   let rec loop n =
     if n = max_n
     then Lwt.return ()
     else
       begin
         _progress t0 n 10000 oc >>= fun () ->
-        let key = _cat "key" n
+        let key = _cat k0 n
         and value = _cat v0 n in
         client # set key value >>= fun () ->
         loop (n+1)
@@ -97,10 +102,11 @@ let _fill client max_n size (t0:float) (oc:Lwt_io.output_channel) =
   in
   loop 0
 
-let _fill_transactions client max_n tx_size size (t0:float) oc =
+let _fill_transactions client max_n tx_size k_size v_size (t0:float) oc =
   Lwt_io.fprintlf oc "(started @ %f)" (Unix.gettimeofday()) >>= fun () ->
   let n_transactions = (max_n + tx_size -1) / tx_size in
-  let v0 = String.make  (size -8) 'x' in
+  let k0 = String.make (k_size -8) '_' in
+  let v0 = String.make (v_size -8) 'x' in
   let rec loop_t i =
     if i = n_transactions then Lwt.return ()
     else
@@ -110,7 +116,7 @@ let _fill_transactions client max_n tx_size size (t0:float) oc =
           else
             let c = i * tx_size + j in
             let value = _cat v0 c in
-            let u = Set (_cat "key" c, value) in
+            let u = Set (_cat k0 c, value) in
             build (u::acc) (j+1)
         in
         _progress t0 i 1000 oc >>= fun () ->
@@ -160,47 +166,50 @@ let _time
 
 
 let benchmark
-      ?(size=10)
+      ?(value_size=10)
+      ?(key_size=12)
       ?(tx_size=100)
       ?(max_n = 1000 * 1000)
       ~with_c
       n_clients
   =
-  let sz =
-    if size < 10 then 10 else size
-  in
+  let v_size = if value_size < 10 then 10 else value_size in
+  let k_size = if key_size < 12 then 12 else key_size in
   let phase_0 client oc =
     client # who_master () >>= fun master ->
     Lwt_io.fprintlf oc "Master %s; size=%i"
       (Log_extra.string_option2s master)
-      sz
+      v_size
   in
   let phase_1 client oc =
-    Lwt_io.fprintlf oc "going to do %i 'sets' of %i bytes" max_n sz >>= fun () ->
-    _time (_fill client max_n sz) oc >>= fun d ->
+    Lwt_io.fprintlf oc "%i 'sets' key_size=%i bytes; value_size=%i bytes"
+      max_n k_size v_size >>= fun () ->
+    _time (_fill client max_n k_size v_size) oc >>= fun d ->
     Lwt_io.fprintlf oc "\nfill %i took: %f" max_n d  >>= fun () ->
     Lwt.return ()
   in
   let phase_2 client oc =
-    Lwt_io.fprintlf oc "\n\ngoing to do %i 'sets' in transactions of size %i"
+    Lwt_io.fprintlf oc "\n\n%i 'sets' in transactions of size %i"
       max_n tx_size >>= fun () ->
-    _time (_fill_transactions client max_n tx_size sz) oc >>= fun d ->
+    _time (_fill_transactions client max_n tx_size k_size v_size) oc >>= fun d ->
     Lwt_io.fprintlf oc "\nfill_transactions %i took : %f" max_n d
   in
   let phase_3 client oc =
-    Lwt_io.fprintlf oc "\n\ngoing to %i gets of random keys" max_n >>= fun () ->
-    _time (_get client max_n ) oc >>= fun d ->
+    Lwt_io.fprintlf oc "\n\n%i `gets` (random) key_size=%i value_size=%i "
+      max_n k_size v_size >>= fun () ->
+    _time (_get client max_n k_size) oc >>= fun d ->
     Lwt_io.fprintlf oc "\nget of %i values (random keys) took: %f" max_n d
   in
   let phase_4 client oc =
     (* ???? >>= fun () -> *)
-    _time (_get_transactions client max_n tx_size sz) oc >>= fun d ->
+    _time (_get_transactions client max_n tx_size k_size v_size) oc >>= fun d ->
     Lwt_io.fprintlf oc
-      "\nmultiget of %i values (random keys) in transactions of size %i took %f"
-      max_n tx_size d
+      "\n%i `multigets` (random) key_size=%i value_size =%i tx_size = %i took %f"
+      max_n k_size v_size tx_size d
   in
   let phase_5 client oc =
-    Lwt_io.fprintlf oc "going to do %i range queries" max_n >>= fun () ->
+    Lwt_io.fprintlf oc "%i `range` queries; key_size = %i"
+      max_n k_size >>= fun () ->
     _time (_range client max_n) oc >>= fun d ->
     Lwt_io.fprintlf oc "random ranges took %f" d
   in
