f274a371a4c509ca0b0c5654ee11329259302169
Nicolas Trangez <ikke@nicolast.be>
2013-08-29 15:02:44 +0200

    Tests: Use client TLS connections in test_basic_tls

--- jenkins/system_tests_quick/075_test_basic_tls.sh
+++ jenkins/system_tests_quick/075_test_basic_tls.sh
@@ -33,6 +33,8 @@ cat > arakoon_tls.ini << EOF
 cluster = arakoon_0, arakoon_1, arakoon_2
 cluster_id = arakoon_tls_test
 tls_ca_cert = $CA_PATH/cacert.pem
+tls_service = true
+tls_service_validate_peer = true
 
 [arakoon_0]
 ip = 127.0.0.1
@@ -62,6 +64,8 @@ tls_cert = $CA_PATH2/arakoon2.pem
 tls_key = $CA_PATH2/arakoon2.key
 EOF
 
+CLIENT_TLS_OPTS="-tls-ca-cert $CA_PATH/cacert.pem -tls-cert $CA_PATH/arakoon2.pem -tls-key $CA_PATH/arakoon2.key"
+
 echo "Test #1: Make sure node listens using TLS & presents cert"
 ./arakoon.native -config arakoon_tls.ini --node arakoon_0 &
 ARAKOON0_PID=$!
@@ -76,7 +80,7 @@ ARAKOON0_PID=$!
 ./arakoon.native -config arakoon_tls.ini --node arakoon_1 &
 ARAKOON1_PID=$!
 sleep 2
-./arakoon.native -config arakoon_tls.ini --who-master
+./arakoon.native -config arakoon_tls.ini $CLIENT_TLS_OPTS --who-master
 kill $ARAKOON0_PID
 kill $ARAKOON1_PID
 sleep 1
@@ -87,19 +91,55 @@ ARAKOON0_PID=$!
 ./arakoon.native -config arakoon_tls.ini --node arakoon_1 &
 ARAKOON1_PID=$!
 sleep 2
-./arakoon.native -config arakoon_tls.ini --who-master
+./arakoon.native -config arakoon_tls.ini $CLIENT_TLS_OPTS --who-master
 kill $ARAKOON1_PID
 sleep 11
-./arakoon.native -config arakoon_tls.ini --who-master 2>&1 | grep "No Master"
+./arakoon.native -config arakoon_tls.ini $CLIENT_TLS_OPTS --who-master 2>&1 | grep "No Master"
 ./arakoon.native -config arakoon_tls.ini --node arakoon_1 &
 ARAKOON1_PID=$!
 sleep 2
-./arakoon.native -config arakoon_tls.ini --who-master
+./arakoon.native -config arakoon_tls.ini $CLIENT_TLS_OPTS --who-master
 kill $ARAKOON0_PID
 kill $ARAKOON1_PID
 sleep 1
 
 echo "Test #4: Make sure certs are validated"
+# Can't use tls_service_validate_peer in this test: client can connect to both
+# nodes, one of them will reject the connection due to certificate mismatch.
+cat > arakoon_tls.ini << EOF
+[global]
+cluster = arakoon_0, arakoon_1, arakoon_2
+cluster_id = arakoon_tls_test
+tls_ca_cert = $CA_PATH/cacert.pem
+
+[arakoon_0]
+ip = 127.0.0.1
+client_port = 4000
+messaging_port = 4010
+home = $NODES_PATH/arakoon_0
+log_level = debug
+tls_cert = $CA_PATH/arakoon0.pem
+tls_key = $CA_PATH/arakoon0.key
+
+[arakoon_1]
+ip = 127.0.0.1
+client_port = 4001
+messaging_port = 4011
+home = $NODES_PATH/arakoon_1
+log_level = debug
+tls_cert = $CA_PATH/arakoon1.pem
+tls_key = $CA_PATH/arakoon1.key
+
+[arakoon_2]
+ip = 127.0.0.1
+client_port = 4002
+messaging_port = 4012
+home = $NODES_PATH/arakoon_2
+log_level = debug
+tls_cert = $CA_PATH2/arakoon2.pem
+tls_key = $CA_PATH2/arakoon2.key
+EOF
+
 ./arakoon.native -config arakoon_tls.ini --node arakoon_0 &
 ARAKOON0_PID=$!
 ./arakoon.native -config arakoon_tls.ini --node arakoon_2 &
