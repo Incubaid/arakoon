c0005f5d2508114dd696bd665c764d9f6c8fc69b
Romain Slootmaekers <romain@incubaid.com>
2013-10-28 17:58:12 +0100

    depends on libsnappy-dev

--- jenkins/base/005_install_ubuntu_dependencies.sh
+++ jenkins/base/005_install_ubuntu_dependencies.sh
@@ -2,6 +2,8 @@
 
 sudo aptitude update || true
 
-for PKG in libssl-dev texlive texlive-latex-extra git python-epydoc graphviz; do
+for PKG in libssl-dev \
+ texlive texlive-latex-extra \
+ git python-epydoc graphviz libsnappy-dev; do
     sudo aptitude install -yVDq $PKG
 done
