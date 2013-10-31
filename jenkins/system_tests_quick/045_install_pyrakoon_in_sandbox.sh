7f49347642b766717d6da0df13319b4d494b6d3b
Nicolas Trangez <ikke@nicolast.be>
2013-10-31 14:38:47 +0100

    Testing

--- jenkins/system_tests_quick/045_install_pyrakoon_in_sandbox.sh
+++ jenkins/system_tests_quick/045_install_pyrakoon_in_sandbox.sh
@@ -7,14 +7,12 @@ pushd pyrakoon
 git describe --all --long --always --dirty
 popd
 
-rm -rf /opt/qbase3/lib/python/site-packages/arakoon/
-mkdir -p /opt/qbase3/lib/python/site-packages/arakoon/
-
 ln -s `pwd`/pyrakoon/pyrakoon /opt/qbase3/lib/python/site-packages/pyrakoon
 
-pushd /opt/qbase3/lib/python/site-packages/arakoon
-touch __init__.py
-cat > Arakoon.py << EOF
-from pyrakoon.compat import ArakoonClient, ArakoonClientConfig
+/opt/qbase3/bin/python << EOF
+import arakoon
+import pyrakoon
+
+print '\'arakoon\' at', inspect.getfile(arakoon)
+print '\'pyrakoon\' at', inspect.getfile(pyrakoon)
 EOF
-popd
