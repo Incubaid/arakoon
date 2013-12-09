0fb3790fd012f819f06634ea5feb730e66c9b145
Jan Doms <jan.doms@gmail.com>
2013-12-09 09:41:56 +0100

    update jenkins seed

--- jenkins/base/008_install_dev_env.sh
+++ jenkins/base/008_install_dev_env.sh
@@ -3,14 +3,13 @@
 which opam > /dev/null || { echo 'opam not found!'; exit 1; }
 { opam remote list | grep Incubaid/opam-repository-devel > /dev/null; } || { opam remote add incubaid-devel -k git git://github.com/Incubaid/opam-repository-devel.git; }
 
-opam switch 4.00.1
+opam switch 4.01.0
 eval `opam config env`
 
 opam update -y
 opam pin camltc none || true
-opam remove -y ounit
-opam install conf-libev 
-opam install -y camlbz2 
+opam install -y conf-libev
+opam install -y camlbz2
 opam install -y "lwt.2.4.3"
 opam install -y "camltc.999"
 opam install -y bisect
