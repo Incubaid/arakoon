0c33b89f1d2e71c346e0380ed9e8d5942ca91276
Nicolas Trangez <ikke@nicolast.be>
2014-01-22 11:02:01 +0100

    Add some labels to bool arguments
    As requested during review of Incubaid/arakoon#313.
    
    See: https://github.com/Incubaid/arakoon/pull/313
    
    Conflicts:
    	src/main/main.ml
    	src/node/dump_store.ml

--- src/node/dump_store.ml
+++ src/node/dump_store.ml
@@ -94,7 +94,7 @@ let inject_as_head fn node_id cfg_fn ~in_place =
       if (not in_place)
       then begin
         Lwt_io.printf "cp %S %S" fn old_head_name >>=fun () ->
-        File_system.copy_file fn old_head_name true
+        File_system.copy_file fn old_head_name ~overwrite:true
       end
       else begin
         Lwt_io.printf "rename %S %S" fn old_head_name >>= fun () ->
