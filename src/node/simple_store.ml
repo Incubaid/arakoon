ea21477baa967c726ec5c5069396e953f40483ca
Jan Doms <jan.doms@gmail.com>
2014-03-01 13:25:10 +0100

    cleanup of some unused functions in simple_store + allow next_prefix to handle the empty string

--- src/node/simple_store.ml
+++ src/node/simple_store.ml
@@ -180,42 +180,22 @@ module type Simple_store = sig
   val copy_store2 : string -> string -> bool -> unit Lwt.t
 end
 
-let _f _pf = function
-  | Some x -> (Some (_pf ^ x))
-  | None -> (Some _pf)
-let _l _pf = function
-  | Some x -> (Some (_pf ^ x))
-  | None -> None
-
-let _filter get_key make_entry fold coll pf =
-  let pl = String.length pf in
-  fold (fun acc entry ->
-      let key = get_key entry in
-      let kl = String.length key in
-      let key' = String.sub key pl (kl-pl) in
-      (make_entry entry key')::acc) [] coll
-
-let filter_keys_list keys =
-  _filter (fun i -> i) (fun e k -> k) List.fold_left keys __prefix
-
-let filter_entries_list entries =
-  _filter (fun (k,v) -> k) (fun (k,v) k' -> (k',v)) List.fold_left entries __prefix
-
-let filter_keys_array entries =
-  _filter (fun i -> i) (fun e k -> k) Array.fold_left entries __prefix
-
 let next_prefix prefix =
-  let next_char c =
-    let code = Char.code c + 1 in
-    match code with
+  if 0 = String.length prefix
+  then
+    None
+  else
+    let next_char c =
+      let code = Char.code c + 1 in
+      match code with
       | 256 -> Char.chr 0, true
       | code -> Char.chr code, false in
-  let rec inner s pos =
-    let c, carry = next_char s.[pos] in
-    s.[pos] <- c;
-    match carry, pos with
+    let rec inner s pos =
+      let c, carry = next_char s.[pos] in
+      s.[pos] <- c;
+      match carry, pos with
       | false, _ -> Some s
       | true, 0 -> None
       | true, pos -> inner s (pos - 1) in
-  let copy = String.copy prefix in
-  inner copy ((String.length copy) - 1)
+    let copy = String.copy prefix in
+    inner copy ((String.length copy) - 1)
