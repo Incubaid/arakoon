998dac64d035f43e58e33526fca171029fa93836
Jan Doms <jan.doms@gmail.com>
2014-03-20 09:52:45 +0100

    update run command for kwik, B, C and D test suites

--- jenkins/B/070_run_tests.sh
+++ jenkins/B/070_run_tests.sh
@@ -2,8 +2,4 @@
 
 echo $PWD
 
-python test_it.py --with-xunit --xunit-file=${PWD}/B.xml \
-  server.left.system_tests_long_left \
-  server.left.test_collapse \
-  server.left.test_concurrent_collapse_fails \
-  server.left.test_copy_db_to_head
+python test_it.py --with-xunit --xunit-file=${PWD}/B.xml ./server/left
--- jenkins/C/070_run_tests.sh
+++ jenkins/C/070_run_tests.sh
@@ -2,10 +2,4 @@
 
 echo $PWD
 
-python test_it.py --with-xunit --xunit-file=${PWD}/C.xml \
-  server.right.system_tests_preferred_masters \
-  server.right.system_tests_anomaly  \
-  server.right.test_catchup_exercises \
-  server.right.system_tests_learner \
-  server.right.test_extension \
-  server.right.system_tests_long_right
+python test_it.py --with-xunit --xunit-file=${PWD}/C.xml ./server/right
