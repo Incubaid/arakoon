05ce0ca7375f945a8492746ffa0517d28b1e72a3
Nicolas Trangez <ikke@nicolast.be>
2014-05-14 17:01:58 +0200

    Add `quickcheck` to dependencies
    As pointed out by @domsj in #413.

--- myocamlbuild.ml
+++ myocamlbuild.ml
@@ -5,6 +5,7 @@ let dependencies = [ "lwt"
                    ; "snappy"
                    ; "ssl"
                    ; "bz2"
+                   ; "quickcheck"
                    ]
 
 (* Enabled compiler warnings, argument for '-w', see `man ocamlc` *)
@@ -30,7 +31,11 @@ let list_dependencies () =
     (pkg, package.Findlib.version, package.Findlib.description)
   in
   let pkgs = List.map query dependencies in
-  let lines = List.map (fun (a, b, c) -> Printf.sprintf "%s\t%s\t%s" a b c) pkgs in
+  let lines = List.map (fun (pkg, version, descr) ->
+    let tabs = if String.length pkg < 8 then "\t\t" else "\t" in
+    Printf.sprintf "%s%s%s\t%s" pkg tabs version descr)
+    pkgs
+  in
   String.concat "\n" lines
 
 let time () =
