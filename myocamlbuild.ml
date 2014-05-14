79a5e1014a5512c4f36d2f56722578e446363499
Nicolas Trangez <ikke@nicolast.be>
2014-05-14 12:19:13 +0200

    Use Findlib to find bisect

--- myocamlbuild.ml
+++ myocamlbuild.ml
@@ -2,11 +2,6 @@ open Ocamlbuild_pack
 open Ocamlbuild_plugin
 open Unix
 
-let run_and_read = Ocamlbuild_pack.My_unix.run_and_read
-
-(* ocamlfind command *)
-let ocamlfind x = S[A"ocamlfind"; x]
-
 let run_cmd cmd =
   try
     let ch = Unix.open_process_in cmd in
@@ -14,7 +9,6 @@ let run_cmd cmd =
     let () = close_in ch in
     line
   with | End_of_file -> "Not available"
-
 
 let output_cmd cmd =
   let acc = ref [] in
@@ -39,19 +33,6 @@ let machine = run_cmd "uname -mnrpio"
 let compiler_version = run_cmd "ocamlopt -version"
 
 let dependencies = output_cmd "opam list -i | grep 'lwt\\|ounit\\|camltc\\|snappy\\|ssl\\|camlbz2'"
-
-let split s ch =
-  let x = ref [] in
-  let rec go s =
-    let pos = String.index s ch in
-    x := (String.before s pos)::!x;
-    go (String.after s (pos + 1))
-  in
-  try
-    go s
-  with Not_found -> !x
-
-let split_nl s = split s '\n'
 
 let time =
   let tm = Unix.gmtime (Unix.time()) in
@@ -85,18 +66,11 @@ let make_version _ _ =
   in
   Cmd (S [A "echo"; Quote(Sh cmd); Sh ">"; P "arakoon_version.ml"])
 
-let before_space s =
+let path_to_bisect () =
   try
-    String.before s (String.index s ' ')
-  with Not_found -> s
-
-let path_to_bisect_instrument () =
-  try
-    let r = run_and_read "ocamlfind query bisect" in
-    let r_stripped = List.hd(split_nl r ) in
-    (* Printf.printf "\n\npath=%s\n\n\n" r_stripped ; *)
-    r_stripped
-  with _ -> "___could_not_find_bisect___"
+    let bisect_pkg = Findlib.query "bisect" in
+    bisect_pkg.Findlib.location
+  with Findlib.Findlib_error _ -> "__could_not_find_bisect__"
 
 let _ = dispatch & function
     | After_rules ->
@@ -134,7 +108,7 @@ let _ = dispatch & function
       flag ["ocaml"; "link"; "use_bisect"]
         (S[A"-package"; A"bisect"]);
       flag ["pp"; "ocaml"; "use_bisect"; "maybe_use_bisect"]
-        (S[A"str.cma"; A(path_to_bisect_instrument()  ^ "/bisect_pp.cmo")]);
+        (S[A"str.cma"; A(path_to_bisect () ^ "/bisect_pp.cmo")]);
 
       flag ["pp";"use_macro";"small_tlogs";
             "file:src/tlog/tlogcommon.ml"] (S[A"-DSMALLTLOG"]);
