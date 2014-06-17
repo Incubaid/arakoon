941fcfffd25956ea97d53e6646c764da9d56c1a6
Jan Doms <jan.doms@gmail.com>
2014-06-17 11:37:40 +0200

    fix CI build with new images

--- jenkins/base/008_install_dev_env.sh
+++ jenkins/base/008_install_dev_env.sh
@@ -3,6 +3,8 @@
 which opam > /dev/null || { echo 'opam not found!'; exit 1; }
 { opam remote list | grep Incubaid/opam-repository-devel > /dev/null; } || { opam remote add incubaid-devel -k git git://github.com/Incubaid/opam-repository-devel.git; }
 
+opam --version
+
 opam switch 4.01.0
 eval `opam config env`
 
