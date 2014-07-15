ba298d2c1391e2fa2ceb44275823f11bd1f1eaa9
Jan Doms <jan.doms@gmail.com>
2014-07-15 11:11:40 +0200

    remove help2man dependency from jenkins/base/005_install_ubuntu_dependencies.sh

--- jenkins/base/005_install_ubuntu_dependencies.sh
+++ jenkins/base/005_install_ubuntu_dependencies.sh
@@ -4,6 +4,6 @@ sudo aptitude update || true
 
 for PKG in libssl-dev \
  texlive texlive-latex-extra \
- git help2man python-epydoc graphviz libsnappy-dev; do
+ git python-epydoc graphviz libsnappy-dev; do
     sudo aptitude install -yVDq $PKG
 done
