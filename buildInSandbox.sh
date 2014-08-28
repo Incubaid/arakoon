7a26c19b582df45f6aa467ed46631aced4b5d6e7
Nicolas Trangez <ikke@nicolast.be>
2014-08-28 13:20:45 +0200

    Link `pyrakoon` into sandbox `site-packages`

--- buildInSandbox.sh
+++ buildInSandbox.sh
@@ -10,6 +10,18 @@ cp pylabs/Compat.py /opt/qbase3/lib/pymonkey/extensions/Compat.py
 mkdir -p /opt/qbase3/var/tests/arakoon_system_tests/
 touch /opt/qbase3/var/tests/arakoon_system_tests/__init__.py
 cp -r pylabs/test/* /opt/qbase3/var/tests/arakoon_system_tests/
-mkdir -p /opt/qbase3/lib/python/site-packages/arakoon/
-cp src/client/python/*.py /opt/qbase3/lib/python/site-packages/arakoon/
+mkdir -p /opt/qbase3/lib/python/site-packages/
+ln -s `pwd`/pylabs/pyrakoon/pyrakoon /opt/qbase3/lib/python/site-packages/pyrakoon
 
+ARAKOON_PACKAGE=/opt/qbase3/lib/python/site-packages/arakoon
+mkdir -p $ARAKOON_PACKAGE
+touch $ARAKOON_PACKAGE/__init__.py
+cat > $ARAKOON_PACKAGE/Arakoon.py << EOF
+from pyrakoon.compat import *
+EOF
+cat > $ARAKOON_PACKAGE/ArakoonExceptions.py << EOF
+from pyrakoon.compat import *
+EOF
+cat > $ARAKOON_PACKAGE/ArakoonProtocol.py << EOF
+from pyrakoon.compat import *
+EOF
