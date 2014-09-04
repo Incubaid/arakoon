dc5cf623662817bd069dc6d244b5e6a8d8458ab5
Pieter Goetschalckx <3.14.e.ter@gmail.com>
2014-09-04 16:16:50 +0200

    Restored buildInSandbox.sh

--- buildInSandbox.sh
+++ buildInSandbox.sh
@@ -2,14 +2,14 @@
 
 ocamlbuild -use-ocamlfind arakoon.native
 
-sudo mkdir -p /opt/qbase3/apps/arakoon/bin/
-sudo cp arakoon.native /opt/qbase3/apps/arakoon/bin/arakoon
-sudo cp -r pylabs/extensions/* /opt/qbase3/lib/pymonkey/extensions/
-sudo cp pylabs/Compat.py /opt/qbase3/lib/pymonkey/extensions/Compat.py
+mkdir -p /opt/qbase3/apps/arakoon/bin/
+cp arakoon.native /opt/qbase3/apps/arakoon/bin/arakoon
+cp -r pylabs/extensions/* /opt/qbase3/lib/pymonkey/extensions/
+cp pylabs/Compat.py /opt/qbase3/lib/pymonkey/extensions/Compat.py
 
-sudo mkdir -p /opt/qbase3/var/tests/arakoon_system_tests/
-sudo touch /opt/qbase3/var/tests/arakoon_system_tests/__init__.py
-sudo cp -r pylabs/test/* /opt/qbase3/var/tests/arakoon_system_tests/
-sudo mkdir -p /opt/qbase3/lib/python/site-packages/arakoon/
-sudo cp src/client/python/*.py /opt/qbase3/lib/python/site-packages/arakoon/
+mkdir -p /opt/qbase3/var/tests/arakoon_system_tests/
+touch /opt/qbase3/var/tests/arakoon_system_tests/__init__.py
+cp -r pylabs/test/* /opt/qbase3/var/tests/arakoon_system_tests/
+mkdir -p /opt/qbase3/lib/python/site-packages/arakoon/
+cp src/client/python/*.py /opt/qbase3/lib/python/site-packages/arakoon/
 
