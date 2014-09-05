e21d0aca6befe350e025084e44693d0ac31588bb
Nicolas Trangez <ikke@nicolast.be>
2014-09-05 17:50:59 +0200

    Disable `epydoc` building

--- jenkins/base/010_build.sh
+++ jenkins/base/010_build.sh
@@ -23,10 +23,4 @@ ocamlbuild -use-ocamlfind arakoon.native arakoon.byte
 echo "Fixup symlinks (absolute to relative)"
 symlinks -c .
 
-mkdir -p doc/python/client
-epydoc --html --output ./doc/python/client --name Arakoon --url http://www.arakoon.org --inheritance listed --graph all src/client/python
-mkdir -p doc/python/client/extension
-epydoc --html --output ./doc/python/client/extension --name "Arakoon PyLabs client extension" --url http://www.arakoon.org --inheritance listed --graph all pylabs/extensions/arakoon_ext/client/*.py
-mkdir -p doc/python/server/extension
-epydoc --html --output ./doc/python/server/extension --name "Arakoon PyLabs server extension" --url http://www.arakoon.org --inheritance listed --graph all pylabs/extensions/arakoon_ext/server/*.py
 cp arakoon.native arakoon
