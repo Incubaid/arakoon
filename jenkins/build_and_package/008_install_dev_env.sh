c92d09c81e0baab64a019b1feff6d7efe874a252
Jan Doms <jan.doms@gmail.com>
2014-10-02 13:32:15 +0200

    update CI env to use ocaml 4.02

--- jenkins/build_and_package/008_install_dev_env.sh
+++ jenkins/build_and_package/008_install_dev_env.sh
@@ -3,11 +3,11 @@
 which opam > /dev/null || { echo 'opam not found!'; exit 1; }
 { opam remote list | grep Incubaid/opam-repository-devel > /dev/null; } || { opam remote add incubaid-devel -k git git://github.com/Incubaid/opam-repository-devel.git; }
 
-opam switch 4.01.0
+opam update -y
+opam switch 4.02.0
 eval `opam config env`
 
 opam remove -y camltc
-opam update -y
 opam install -y conf-libev
 opam install -y camlbz2
 opam install -y lwt.2.4.4
--- jenkins/kwik/008_install_dev_env.sh
+++ jenkins/kwik/008_install_dev_env.sh
@@ -3,10 +3,10 @@
 which opam > /dev/null || { echo 'opam not found!'; exit 1; }
 { opam remote list | grep Incubaid/opam-repository-devel > /dev/null; } || { opam remote add incubaid-devel -k git git://github.com/Incubaid/opam-repository-devel.git; }
 
-opam switch 4.01.0
+opam update -y
+opam switch 4.02.0
 eval `opam config env`
 
-opam update -y
 opam install -y ssl
 opam install -y conf-libev
 opam install -y camlbz2
