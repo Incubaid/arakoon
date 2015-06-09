1a740fee0ac62294d47055cd14f52592bce5b460
Jan Doms <jan.doms@gmail.com>
2015-06-09 13:14:39 +0200

    fix the nocrypto version

--- jenkins/package_deb/008_install_dev_env.sh
+++ jenkins/package_deb/008_install_dev_env.sh
@@ -10,4 +10,4 @@ eval `opam config env`
 opam install -y \
     conf-libev.4-11 camlbz2.0.6.0 lwt.2.4.8 camltc.0.9.2 \
     ssl.0.5.0 bisect.1.3 quickcheck.1.0.2 \
-    ounit.2.0.0
+    ounit.2.0.0 nocrypto.0.4.0
