556412296b3a42379c7f25d0edd66eee45820352
Romain Slootmaekers <romain.slootmaekers@cloudfounders.com>
2015-07-13 15:58:12 +0200

    set versions for all opam packages.

--- jenkins/008_install_dev_env.sh
+++ jenkins/008_install_dev_env.sh
@@ -1,19 +1,19 @@
 #!/bin/bash -xue
 
 which opam > /dev/null || { echo 'opam not found!'; exit 1; }
-{ opam remote list | grep Incubaid/opam-repository-devel > /dev/null; } || { opam remote add incubaid-devel -k git git://github.com/Incubaid/opam-repository-devel.git; }
 
-opam update -y
 opam switch 4.02.1
 eval `opam config env`
 
 # do this in 1 step, otherwise, you might not get what you want
-opam install -y "ssl.0.4.7" \
-  conf-libev \
-  camlbz2 \
-  snappy \
-  "lwt.2.4.8" \
-  "camltc.999" \
-  bisect \
-  quickcheck \
-  nocrypto.0.4.0
+opam install -y ssl.0.5.0 \
+  conf-libev.4-11 \
+  camlbz2.0.6.0 \
+  snappy.0.1.0 \
+  lwt.2.4.8 \
+  camltc.0.9.2 \
+  bisect.1.3 \
+  quickcheck.1.0.2 \
+  nocrypto.0.4.0 \
+  sexplib.112.35.00 \
+  uuidm.0.9.5
