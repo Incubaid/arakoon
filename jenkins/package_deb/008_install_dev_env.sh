044893f2aa544fbdf49d19dd00029f7525787cff
Romain Slootmaekers <romain.slootmaekers@cloudfounders.com>
2015-07-13 16:36:10 +0200

    also fix ctypes and friends

--- jenkins/package_deb/008_install_dev_env.sh
+++ jenkins/package_deb/008_install_dev_env.sh
@@ -12,6 +12,9 @@ opam install -y ssl.0.5.0 \
   snappy.0.1.0 \
   lwt.2.4.8 \
   camltc.0.9.2 \
+  cstruct.1.7.0 \
+  ctypes.0.4.1 \
+  ctypes-foreign.0.4.0 \
   bisect.1.3 \
   quickcheck.1.0.2 \
   nocrypto.0.4.0 \
