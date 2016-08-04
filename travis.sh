f85e0a197b78744c39a9fe925b63ae84de0ef420
Jan Doms <jan.doms@gmail.com>
2016-08-04 16:50:18 +0200

    try speedup the travis build

--- travis.sh
+++ travis.sh
@@ -3,13 +3,38 @@
 install () {
     echo "Running 'install' phase"
 
+    date
+
+    if [[ -e ~/cache/image.tar.gz ]];
+    then docker load -i ~/cache/image.tar.gz; fi
+
+    date
+
     ./docker/run.sh ubuntu-16.04 build
+
+    date
 }
 
 script () {
     echo "Running 'script' phase"
 
+    date
+
     ./docker/run.sh ubuntu-16.04 unit-travis
+
+    date
+}
+
+before_cache () {
+    echo "Running 'before_cache' phase"
+
+    date
+
+    mkdir ~/cache || true
+    docker save -o ~/cache/image.tar.gz arakoon_ubuntu-16.04
+    ls -ahl ~/cache
+
+    date
 }
 
 case "$1" in
@@ -18,6 +43,9 @@ case "$1" in
         ;;
     script)
         script
+        ;;
+    before_cache)
+        before_cache
         ;;
     *)
         echo "Usage: $0 {install|script}"
