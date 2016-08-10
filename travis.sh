8ba4f64686f457310788474d36a01591469881df
Jan Doms <jan.doms@gmail.com>
2016-08-10 08:40:37 +0200

    one more change for travis

--- travis.sh
+++ travis.sh
@@ -8,15 +8,25 @@ install () {
     if [[ -e ~/cache/image.tar.gz ]];
     then docker load -i ~/cache/image.tar.gz; fi
 
-    START_BUILD=date
-    export START_BUILD
+    START_BUILD=$(date +%s.%N)
     echo $START_BUILD
 
     ./run_with_timeout_and_progress.sh 9000 ./docker/run.sh ubuntu-16.04 clean
 
-    END_BUILD=date
-    export END_BUILD
+    END_BUILD=$(date +%s.%N)
     echo $END_BUILD
+
+    DIFF=$(echo "$END_BUILD - $START_BUILD" | bc)
+    if [ $DIFF \> 5 ]
+    then
+        df -h
+        mkdir ~/cache || true
+        rm -f ~/cache/image.tar.gz
+        docker save -o ~/cache/image.tar.gz arakoon_ubuntu-16.04
+        ls -ahl ~/cache;
+    else
+        echo Building of container took only $DIFF seconds, not updating cache this time;
+    fi
 }
 
 script () {
@@ -29,33 +39,12 @@ script () {
     date
 }
 
-before_cache () {
-    echo "Running 'before_cache' phase"
-
-    date
-
-    DIFF=$(echo "$END_BUILD - $START_BUILD" | bc)
-    if [ $DIFF \> 5 ]
-    then
-        mkdir ~/cache || true
-        docker save -o ~/cache/image.tar.gz arakoon_ubuntu-16.04
-        ls -ahl ~/cache;
-    else
-        echo Building of container took only $DIFF seconds, not updating cache this time;
-    fi
-
-    date
-}
-
 case "$1" in
     install)
         install
         ;;
     script)
         script
-        ;;
-    before_cache)
-        before_cache
         ;;
     *)
         echo "Usage: $0 {install|script}"
