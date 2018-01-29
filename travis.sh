Incubaid/arakoon

commit f46623fb6d77d0f05e62c95da5caeaa8bdc27f75

Move ./run_with_timeout_and_progress.sh into travis.sh

travis.sh
@@ -1,5 +1,26 @@
 #!/bin/bash -xue
 
+timeout_with_progress () {
+    OUTPUT=`mktemp`
+    timeout "$@" > $OUTPUT 2>&1 &
+    PID=$!
+
+    echo $PID
+
+    while kill -0 $PID 2>/dev/null
+    do
+	echo -ne .
+	sleep 1
+    done
+
+    wait $PID
+    RESULT=$?
+
+    tail -n256 $OUTPUT
+
+    return $RESULT
+}
+
 install () {
     echo "Running 'install' phase"
 
@@ -14,7 +35,7 @@ install () {
     START_BUILD=$(date +%s.%N)
     echo $START_BUILD
 
-    ./run_with_timeout_and_progress.sh 9000 ./docker/run.sh ubuntu-16.04 clean
+    timeout_with_progress 9000 ./docker/run.sh ubuntu-16.04 clean
 
     END_BUILD=$(date +%s.%N)
     echo $END_BUILD
@@ -40,7 +61,7 @@ script () {
 
     date
 
-    ./run_with_timeout_and_progress.sh 9000 ./docker/run.sh ubuntu-16.04 unit
+    timeout_with_progress 9000 ./docker/run.sh ubuntu-16.04 unit
 
     date
 }

run_with_timeout_and_progress.sh
@@ -1,23 +1,0 @@
-#!/bin/bash -u
-
-TIMEOUT=$1
-COMMAND=$2
-shift 2
-
-timeout $TIMEOUT $COMMAND $@ > output 2>&1 &
-PID=$!
-
-echo $PID
-
-while kill -0 $PID 2>/dev/null
-do
-    echo -ne .
-    sleep 1
-done
-
-wait $PID
-RESULT=$?
-
-tail -n256 output
-
-exit $RESULT

commit a4686ef48dbdf26089dfd28cd0dda3af7dff22e1

Fix progress printing broken in f46623f

travis.sh
@@ -1,6 +1,7 @@
 #!/bin/bash -xue
 
-timeout_with_progress () {
+timeout_with_progress () (
+    set +x
     OUTPUT=`mktemp`
     timeout "$@" > $OUTPUT 2>&1 &
     PID=$!
@@ -19,7 +20,7 @@ timeout_with_progress () {
     tail -n256 $OUTPUT
 
     return $RESULT
-}
+)
 
 install () {
     echo "Running 'install' phase"