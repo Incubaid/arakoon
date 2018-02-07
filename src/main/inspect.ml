aea5e5ce55421393d694ecc247f39df57301469f
Jan Doms <jan.doms@gmail.com>
2018-02-07 16:40:59 +0100

    expand inspect-cluster command a bit more

--- src/main/inspect.ml
+++ src/main/inspect.ml
@@ -43,7 +43,8 @@ let inspect_cluster ~tls (cfg : Node_cfg.cluster_cfg) =
         ~fsync:false node_name ~fsync_tlog_dir:false
       >>= fun tlc ->
 
-      S.make_store ~lcnum:1024 ~ncnum:512 (path ^ "/store.db") ~cluster_id
+      let db_path = path ^ "/store.db" in
+      S.make_store ~lcnum:1024 ~ncnum:512 db_path ~cluster_id
       >>= fun store ->
 
       Catchup.catchup_tlog
@@ -56,14 +57,14 @@ let inspect_cluster ~tls (cfg : Node_cfg.cluster_cfg) =
         ((module S), store, tlc) >>= fun tlog_i ->
 
       Lwt_log.info_f "Got head & tlogs for %s, i=%Li" node_name tlog_i >>= fun () ->
-      Lwt.return (node_name, store, tlc, tlog_i)
+      Lwt.return (node_name, db_path, store, tlc, tlog_i)
     )
     cfg.cfgs
   >>= fun nodes ->
 
   let minimum_last_i =
     List.fold_left
-      (fun minimum (_, _, _, current) -> min minimum current)
+      (fun minimum (_, _, _, _, current) -> min minimum current)
       Int64.max_int
       nodes
   in
@@ -71,7 +72,7 @@ let inspect_cluster ~tls (cfg : Node_cfg.cluster_cfg) =
   Lwt_log.info_f "Got all heads & tlogs, minimal i=%Li" minimum_last_i >>= fun () ->
 
   Lwt_list.iter_s
-    (fun (node_name, store, tlc, _) ->
+    (fun (node_name, db_path, store, tlc, _) ->
       Catchup.catchup_store
         ~stop:(ref false)
         node_name
@@ -79,12 +80,45 @@ let inspect_cluster ~tls (cfg : Node_cfg.cluster_cfg) =
         minimum_last_i >>= fun () ->
       S.close store >>= fun () ->
 
-      tlc # close ()
+      tlc # close () >>= fun () ->
+
+      let read_only = true in
+      Local_store.make_store ~lcnum:1024 ~ncnum:512 read_only db_path
+      >>= fun store ->
+
+      File_system.with_fd
+        (db_path ^ ".dump")
+        ~flags:[ Unix.O_WRONLY; Unix.O_CREAT; Unix.O_TRUNC; Unix.O_NONBLOCK; Unix.O_EXCL; ]
+        ~perm:0o644
+        (fun fd_out ->
+          let ch_out = Lwt_io.of_fd ~mode:Lwt_io.output fd_out in
+          let open Camltc in
+          Hotc.with_cursor
+            (Hotc.get_bdb store.Local_store.db)
+            (fun bdb cursor ->
+              let () = Bdb.first bdb cursor in
+              let rec loop () =
+                let key = Bdb.key bdb cursor in
+                let value = Bdb.value bdb cursor in
+                Lwt_io.fprintlf ch_out "key=%S value=%S" key value >>= fun () ->
+                let continue =
+                  try Bdb.next bdb cursor; true
+                  with Not_found -> false
+                in
+                if continue
+                then loop ()
+                else Lwt_io.fprintlf ch_out "Finished iterating over db"
+              in
+              loop ()
+            ) >>= fun () ->
+          Lwt_io.flush ch_out
+        )
     )
     nodes >>= fun () ->
 
-  (* - diff already, tell which are same
-   * - show diff cmds
-   *)
-
-  Lwt.return ()
+  Lwt_io.printlf "\nnow run the following commands to inspect the differences (if any)" >>= fun () ->
+  let _, first, _, _, _ = List.hd nodes in
+  Lwt_list.iter_s
+    (fun (_, db_path, _, _, _) ->
+      Lwt_io.printlf "diff %s.dump %s.dump" first db_path)
+    (List.tl nodes)
