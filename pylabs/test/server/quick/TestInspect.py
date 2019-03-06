c09c0e2a935346c64675bf941b3b93637cfe7f61
Romain Slootmaekers <romain.slootmaekers@openvstorage.com>
2019-03-06 09:39:38 +0100

    move inspect to common to make it usable outside of test

--- pylabs/test/server/quick/TestInspect.py
+++ pylabs/test/server/quick/TestInspect.py
@@ -6,55 +6,6 @@
 import os
 import logging
 from .. import system_tests_common as C
-import time
-from Compat import X
-
-def compare_files(fn0, fn1):
-    with open(fn0, 'r') as f0:
-        with open(fn1, 'r') as f1:
-            go_on = True
-            count = 0
-            while go_on:
-                f0_line = f0.readline()
-                f1_line = f1.readline()
-                logging.debug("l0:%s\nl1:%s\n", f0_line.strip(), f1_line.strip())
-                if f0_line <> f1_line :
-                    raise Exception("%s and %s diff on line %i" % (fn0,fn1, count))
-                if len(f0_line) == 0:
-                    assert(len(f1_line) == 0)
-                    go_on = False
-                count = count + 1
-
-
-def inspect_cluster(cluster):
-
-    # cleanup inspect dumps
-    cluster_id = cluster._getClusterId()
-    node_names = cluster.listLocalNodes()
-    for node_name in node_names:
-        dn = '%s/%s/' % (cluster_id, node_name,)
-        logging.info( "deleting %s", dn)
-        X.removeDirTree(dn)
-
-    cfg_fn = cluster._getConfigFileName()
-    r = C.call_arakoon("--inspect-cluster", "-config", "%s.cfg" % cfg_fn)
-    
-
-    dump_paths = []
-    for node_name in node_names:
-        dump_path = './%s/%s/store.db.dump' % (cluster_id, node_name)
-        dump_paths.append(dump_path)
-
-    size = len(node_names)
-    for i in xrange(size -1):
-        for j in xrange(i+1, size):
-            fn0 = dump_paths[i]
-            fn1 = dump_paths[j]
-            compare_files(fn0,fn1)
-            logging.info("%s == %s",fn0,fn1)
-    logging.info("inspect_cluster(%s): ok", cluster_id)
-
-
 
 @C.with_custom_setup(C.setup_3_nodes_mini, C.basic_teardown)
 def test_inspect():
@@ -83,9 +34,6 @@ def test_inspect():
     r = C.call_arakoon("--dev-set-store", head_file_name, "@AAAAA", "AAA" * 300)
     r = C.call_arakoon("--dev-set-store", head_file_name, "@zzzzz", "ZZZ" * 300)
 
-    
-
-    
     C.start_all()
 
     logging.info("%s's head has been updated", node_id)
@@ -95,7 +43,6 @@ def test_inspect():
         k = "key_%04i" % i
         cli.set(k,k)
 
-    #time.sleep(100)
     logging.info("inspect .... which should fail!")
     ok = False
     try:
--- pylabs/test/server/system_tests_common.py
+++ pylabs/test/server/system_tests_common.py
@@ -508,11 +508,11 @@ def get_memory_usage(node_name):
     pid = cluster._getPid(node_name )
     if pid is None:
         return 0
-    cmd = ["ps", "-p", pid, "-o", "vsz"]
+    cmd = ["ps", "-p", str(pid), "-o", "vsz"]
     try:
         stdout = X.subprocess.check_output( cmd )
-    except:
-        logging.error( "Coud not determine memory usage: %s" % stderr )
+    except Exception,e:
+        logging.error( "Coud not determine memory usage: %s" % e)
         return 0
 
     try:
@@ -1343,3 +1343,49 @@ def reverse_range_entries_scenario(start_suffix):
         assert_equals(kv_list[0][0], 'key_000000001099')
     except Exception, ex:
         raise ex
+
+
+def compare_files(fn0, fn1):
+    with open(fn0, 'r') as f0:
+        with open(fn1, 'r') as f1:
+            go_on = True
+            count = 0
+            while go_on:
+                f0_line = f0.readline()
+                f1_line = f1.readline()
+                logging.debug("l0:%s\nl1:%s\n", f0_line.strip(), f1_line.strip())
+                if f0_line <> f1_line :
+                    raise Exception("%s and %s diff on line %i" % (fn0,fn1, count))
+                if len(f0_line) == 0:
+                    assert(len(f1_line) == 0)
+                    go_on = False
+                count = count + 1
+
+
+def inspect_cluster(cluster):
+
+    # cleanup inspect dumps
+    cluster_id = cluster._getClusterId()
+    node_names = cluster.listLocalNodes()
+    for node_name in node_names:
+        dn = '%s/%s/' % (cluster_id, node_name,)
+        logging.info( "deleting %s", dn)
+        X.removeDirTree(dn)
+
+    cfg_fn = cluster._getConfigFileName()
+    r = call_arakoon("--inspect-cluster", "-config", "%s.cfg" % cfg_fn)
+
+
+    dump_paths = []
+    for node_name in node_names:
+        dump_path = './%s/%s/store.db.dump' % (cluster_id, node_name)
+        dump_paths.append(dump_path)
+
+    size = len(node_names)
+    for i in xrange(size -1):
+        for j in xrange(i+1, size):
+            fn0 = dump_paths[i]
+            fn1 = dump_paths[j]
+            compare_files(fn0,fn1)
+            logging.info("%s == %s",fn0,fn1)
+    logging.info("inspect_cluster(%s): ok", cluster_id)
